40c84f3e22b122a23add1cdcf8bc0cbcb66afddc
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2024-11-19 00:36:06 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int storey) {
+    int answer = 0;
+    
+    while(storey > 0)
+    {
+        int cur = storey % 10;
+        
+        if(cur > 5 || (cur == 5 && storey / 10 % 10 >= 5))
+        {
+            answer += 10 - cur;
+            storey += cur;
+        }
+        
+        // else if(cur == 5 && storey / 10 % 10 == 5)
+        
+        else
+        {
+            answer += cur;
+            storey -= cur;
+        }
+        
+        storey /= 10;
+    }
+    
+    return answer;
+}
