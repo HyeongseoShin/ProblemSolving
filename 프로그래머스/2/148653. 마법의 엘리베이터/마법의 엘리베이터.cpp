2a2bec45b34eecffa1f35ac0b4aea980beb13280
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2024-11-19 00:39:29 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
@@ -1,3 +1,7 @@
+/*
+floating signal ==> 수를 0으로 나눌 때 문제 생김
+
+*/
 #include <string>
 #include <vector>
 #include <bits/stdc++.h>
@@ -9,15 +13,16 @@ int solution(int storey) {
     
     while(storey > 0)
     {
+        // 그냥 / 10만 하면 됨 자릿수가 날라가서
+        // 왜 storey - storey % 10 이런 걸 했었을까
         int cur = storey % 10;
         
+        // 현재 수가 5이고 5이상의 수가 연속적일 때
         if(cur > 5 || (cur == 5 && storey / 10 % 10 >= 5))
         {
             answer += 10 - cur;
             storey += cur;
         }
-        
-        // else if(cur == 5 && storey / 10 % 10 == 5)
         
         else
         {
@@ -25,6 +30,8 @@ int solution(int storey) {
             storey -= cur;
         }
         
+        // 그냥 / 10만 하면 됨 자릿수가 날라가서
+        // 왜 storey - storey % 10 이런 걸 했었을까
         storey /= 10;
     }
     
