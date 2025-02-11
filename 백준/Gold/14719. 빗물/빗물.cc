1b7bf0fa295aa7885f0ce0edb250c4256e3de996
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-02-11 17:56:06 +0900

    [Gold V] Title: 빗물, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.cc
+++ 백준/Gold/14719. 빗물/빗물.cc
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int board[512][512];
+int blocks[501];
 
 int w, h;
 
@@ -22,28 +22,31 @@ int main()
 
         cin >> x;
 
-        for(int j = 0; j < x; j++)
-        {
-            board[w - 1 - j][i] = 1;
-        }   
+        blocks[i] = x;
     }
 
-    for(int i = 0; i < w; i++)
+    for(int i = 0; i < h; i++)
     {
-        int cur = 0;
-        int st = 0;
-        for(int j = 0; j < h; j++)
+        int l = 0;
+        int r = 0;
+
+        // 나보다 왼쪽에서 가장 높은 블럭의 층 수
+        for(int j = 0; j < i; j++)
         {
-            if(board[i][j] == 0 && board[i][st] != 0) cur++;
-            else if(board[i][j] == 1)
-            {
-                if(cur != 0)
-                {
-                    ans += cur;
-                    cur = 0;
-                }
-                st = j;
-            }
+            l = max(l, blocks[j]);
+        }
+
+        // 나보다 오른쪽에서 가장 높은 블럭의 층 수
+        for(int j = i; j < h; j++)
+        {
+            r = max(r, blocks[j]);
+        }
+
+        // 내가 왼쪽 오른쪽 최댓값보다 작다면 빗물이 고일 수 있음
+        if(blocks[i] < l && blocks[i] < r)
+        {
+            // 둘 중 작은 블록의 층 - 내 층
+            ans += min(l, r) - blocks[i];
         }
     }
 
