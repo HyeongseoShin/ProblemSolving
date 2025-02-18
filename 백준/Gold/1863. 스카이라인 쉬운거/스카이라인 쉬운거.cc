5ec8ebc2256cf592803f2e5bbbb2bd998d8cacb1
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-02-18 11:33:51 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.cc
@@ -0,0 +1,39 @@
+// https://www.acmicpc.net/problem/1863
+
+// 7% 틀렸습니다
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+stack<int> stk; // x 좌표는 필요 없음
+
+int ans = 0;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    for(int i = 0; i <= n; i++)
+    {
+        int x, y;
+
+        if(i < n) cin >> x >> y;
+        else y = 0; // 스택에 남아 있는 좌표 처리 위해 마지막 값에 y = 0 대입
+
+        while(!stk.empty() && stk.top() >= y)
+        {
+            if(stk.top() > y) ans++;
+            stk.pop();
+        }
+
+        stk.push(y);
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
