77636de2355b81f3df2bc87634aba0635f1d928b
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-02-22 12:19:14 +0900

    [Gold IV] Title: 줄세우기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.cc
@@ -0,0 +1,45 @@
+// https://www.acmicpc.net/problem/2631
+// 스스로 못풂
+// LIS?
+// DP
+
+// 4% 틀렸습니다
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int v[202];
+int dp[202]; // dp[i] = i번째에서 끝나는 최장 증가 수열의 길이
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        cin >> v[i];
+    }
+
+    int longest = 0;
+    for(int i = 1; i <= n; i++)
+    {
+        dp[i] = 1;
+        for(int j = 1; j < i; j++)
+        {
+            if(v[i] > v[j])
+            {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+
+        longest = max(longest, dp[i]);
+    }
+
+    cout << n - longest << "\n";
+
+    return 0;
+}
