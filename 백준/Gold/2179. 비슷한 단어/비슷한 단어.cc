d5b556f99eea6cc4dc9e483ac5a999c69da5efbb
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-02-22 15:09:50 +0900

    [Gold IV] Title: 비슷한 단어, Time: 44 ms, Memory: 12068 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.cc
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.cc
@@ -1,4 +1,3 @@
-// https://www.acmicpc.net/problem/2179
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -6,10 +5,10 @@ using namespace std;
 int n;
 vector<string> v;
 
-string s;
-string t;
-
 int maxLen = 0;
+
+set<string> st;
+unordered_map<string, vector<int>> um;
 
 int main()
 {
@@ -18,40 +17,49 @@ int main()
 
     cin >> n;
 
+    v.resize(n, "");
+
+
+    // Map을 이용한 풀이
+    int maxLen = 0;
     for(int i = 0; i < n; i++)
     {
-        string input;
-        cin >> input;
+        cin >> v[i];
 
-        v.push_back(input);
-    }
-
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = i; j < n; j++)
+        if(st.find(v[i]) == st.end()) // 중복된 문자열 저장 X
         {
-            if(v[i] == v[j]) continue;
+            st.insert(v[i]);
 
-            int idx = 0;
-            int cnt = 0;
-
-            while(idx < v[i].length() && v[i][idx] == v[j][idx])
+            string tmp = "";
+            for(int j = 0; j < v[i].length(); j++) // 한 글자씩 쪼개서 맵에 저장
             {
-                idx++;
-                cnt++;
-            }
+                tmp += v[i][j];
+                um[tmp].push_back(i);
 
-            if(cnt > maxLen)
-            {
-                s = v[i];
-                t = v[j];
-                maxLen = cnt;
+                if(um[tmp].size() > 1 && maxLen < tmp.length()) // tmp를 접두사로 하는 단어가 하나 이상이면 maxLen 갱신
+                {
+                    maxLen = tmp.length();
+                }
             }
         }
     }
 
-    cout << s << "\n";
-    cout << t << "\n";
+    int order = 20002;
+    string ans = "";
+    for(auto [s, sz] : um)
+    {
+        if(s.length() != maxLen) continue;
+        if(sz.size() < 2) continue;
+        
+        if(sz[0] < order)
+        {
+            order = sz[0];
+            ans = s;
+        }
+    }
 
+    cout << v[um[ans][0]] << "\n";
+    cout << v[um[ans][1]] << "\n";
+    
     return 0;
 }
