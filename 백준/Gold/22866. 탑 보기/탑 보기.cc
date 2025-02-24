90a14eae201842d60e7c9c99eef0ed72e90aa7c0
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-02-24 12:17:54 +0900

    [Gold III] Title: 탑 보기, Time: 28 ms, Memory: 3724 KB -BaekjoonHub

---
+++ 백준/Gold/22866. 탑 보기/탑 보기.cc
@@ -0,0 +1,81 @@
+// 스택
+// 오 -> 왼 순회 : 나보다 오른쪽에 있는 건물들 중 볼 수 있는 건물
+// 왼 -> 오 순회 : 나보다 왼쪽에 있는 건물들 중 볼 수 있는 건물
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, l;
+
+int buildings[100001];
+
+int near[100001];
+int cnt[100001];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        cin >> buildings[i];
+    }
+
+    // 오 -> 왼 순회
+    stack<int> stk;
+
+    for(int i = n; i >= 1; i--)
+    {
+        while(!stk.empty() && buildings[i] >= buildings[stk.top()])
+        {
+            stk.pop();
+        }
+
+        if(!stk.empty())
+        {
+            near[i] = stk.top();
+        }
+
+        cnt[i] += stk.size();
+        
+        stk.push(i);
+    }
+
+    // 스택 초기화
+    while(!stk.empty())
+    {
+        stk.pop();
+    }
+
+    //  왼 -> 오 순회
+    for(int i = 1; i <= n; i++)
+    {
+        while(!stk.empty() && buildings[i] >= buildings[stk.top()])
+        {
+            stk.pop();
+        }
+
+        if(!stk.empty())
+        {
+            if(abs(i - stk.top()) <= abs(i - near[i]))
+            {
+                near[i] = stk.top();
+            }
+        }
+
+        cnt[i] += stk.size();
+        
+        stk.push(i);
+    }
+
+    for(int i = 1; i <= n; i++)
+    {
+        if(cnt[i] == 0) cout << cnt[i] << "\n";
+        else cout << cnt[i] << " " << near[i] << "\n";
+    }
+
+    return 0;
+}
