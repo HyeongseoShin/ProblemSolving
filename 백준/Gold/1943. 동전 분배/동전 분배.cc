92c2a1e6e05182ba49b231b5e48424adac54fa96
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-02-25 12:28:57 +0900

    [Gold II] Title: 동전 분배, Time: 204 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.cc
+++ 백준/Gold/1943. 동전 분배/동전 분배.cc
@@ -1,198 +1,61 @@
-#include <iostream>
-        
-        
-          
-          #include <vector>
-        
-        
-          
-          #include <cstring>
-        
-        
-          
-          using namespace std;
-        
-        
-          
-          #define MAX_N 50001
-        
-        
-          
-          
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, test = 3;
+
+vector<pair<int, int>> coins;
+
+int dp[50001]; // dp[i] : 금액 i를 만들 수 있으면 1, 아니면 0
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    while(test--)
+    {
+        cin >> n;
+        int sum = 0;
+
+        coins.clear();
+        for(int i = 0; i < n; i++)
+        {
+            int a, b;
+            cin >> a >> b;
+
+            coins.push_back({a, b});
+
+            sum += (a * b);
+        }
+
+        // 총합이 홀수면 불가능
+        if(sum % 2 == 1)
+        {
+            cout << "0\n";
+            continue;
+        }
+
+        fill(dp, dp + 50001, 0);
+        dp[0] = 1;
+
+        for(auto [type, cnt] : coins)
+        {
+            for(int i = sum / 2; i >= type; i--)
+            {
+                if(dp[i - type] != 0)
+                {
+                    for(int j = 0; (j < cnt) && (i + j * type <= sum / 2); j++)
+                    {
+                        dp[i + j * type] = 1;
+                    }
+                }
+            }
+        }
 
         
-        
-          
-          int repeat = 3;
-        
-        
-          
-          int dp[MAX_N] = { 0, };
-        
-        
-          
-          
+        cout << dp[sum / 2] << "\n";
+    }
 
-        
-        
-          
-          int main() {
-        
-        
-          
-          	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-        
-        
-          
-          
-
-        
-        
-          
-          	while (repeat--) {
-        
-        
-          
-          		int total_cost = 0, coins_kind = 0;
-        
-        
-          
-          		vector<pair<int, int> > coins;
-        
-        
-          
-          
-
-        
-        
-          
-          		cin >> coins_kind;
-        
-        
-          
-          		int value, count;
-        
-        
-          
-          		for (int i = 0; i < coins_kind; i++) {
-        
-        
-          
-          			cin >> value >> count;
-        
-        
-          
-          			coins.push_back({ value, count });
-        
-        
-          
-          			total_cost += value * count;
-        
-        
-          
-          		}
-        
-        
-          
-          
-
-        
-        
-          
-          		if (total_cost % 2 == 1) {
-        
-        
-          
-          			cout << "0\n";
-        
-        
-          
-          		}
-        
-        
-          
-          		else {
-        
-        
-          
-          			memset(dp, 0, sizeof(dp));
-        
-        
-          
-          			dp[0] = 1;
-        
-        
-          
-          
-
-        
-        
-          
-          			for (auto& next : coins) {
-        
-        
-          
-          				for (int i = 50000; i >= next.first; i--) {
-        
-        
-          
-          					if (dp[i - next.first] != 0) {
-        
-        
-          
-          						for (int j = 0; (j < next.second) && (i + j * next.first <= 50000); j++) {
-        
-        
-          
-          							dp[i + j * next.first] = 1;
-        
-        
-          
-          						}
-        
-        
-          
-          					}
-        
-        
-          
-          				}
-        
-        
-          
-          			}
-        
-        
-          
-          
-
-        
-        
-          
-          			cout << dp[total_cost / 2] << '\n';
-        
-        
-          
-          		}
-        
-        
-          
-          
-
-        
-        
-          
-          	}
-        
-        
-          
-          
-
-        
-        
-          
-          	return 0;
-        
-        
-          
-          }
+    return 0;
+}
