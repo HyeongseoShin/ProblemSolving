9db2b457f9dec4ab1ac5352f98aff1542a3a641f
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-03-04 22:43:17 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 8 ms, Memory: 2332 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
@@ -1,56 +1,51 @@
-// https://www.acmicpc.net/problem/4485
-
-// 다익스트라
-
 #include <bits/stdc++.h>
+
+#define MAX 1000001
 
 using namespace std;
 
-int board[200][200];
-int dst[200][200];
+int cave[200][200];
+int dist[200][200];
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
-void BFS(int x, int y, int sz)
-{
-    queue<pair<int, int>> q;
-    q.push({x, y});
-    dst[x][y] = board[x][y];
+int n;
 
-    while(!q.empty())
+void BFS(int x, int y)
+{
+    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
+
+    dist[x][y] = cave[x][y];
+    pq.push({dist[x][y], x, y});
+
+    while(!pq.empty())
     {
-        auto [curX, curY] = q.front();
-        q.pop();
+        auto [curDist, curX, curY] = pq.top();
+        pq.pop();
 
         for(int i = 0; i < 4; i++)
         {
             int nX = curX + dx[i];
             int nY = curY + dy[i];
 
-            if(nX < 0 || nX >= sz || nY < 0 || nY >= sz) continue;
+            if(nX < 0 || nX >= n || nY < 0 || nY >= n) continue;
 
-            // Point: 무조건 재방문이 불가능한 것이 아님!
-            // 최소 거리 업데이트 가능하면 방문 후 거리 업데이트
-            if(dst[nX][nY] <= dst[curX][curY] + board[nX][nY]) continue;
-
-            dst[nX][nY] = dst[curX][curY] + board[nX][nY];
-
-            q.push({nX, nY});
+            if(dist[nX][nY] > curDist + cave[nX][nY])
+            {
+                dist[nX][nY] = curDist + cave[nX][nY];
+                pq.push({dist[nX][nY], nX, nY});
+            }
         }
     }
 }
+
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    int idx = 1;
+    int t = 1;
 
     while(true)
     {
-        int n;
-
         cin >> n;
 
         if(n == 0) break;
@@ -59,17 +54,15 @@ int main()
         {
             for(int j = 0; j < n; j++)
             {
-                cin >> board[i][j];
-                dst[i][j] = INT_MAX;
+                dist[i][j] = MAX;
+                cin >> cave[i][j];
             }
         }
 
-        BFS(0, 0, n);
+        BFS(0, 0);
 
-        cout << "Problem " << idx << ": " << dst[n-1][n-1] << "\n";
-        idx++;
+        cout << "Problem " << t << ": " << dist[n-1][n-1] << "\n";
 
+        t++;
     }
-
-    return 0;
 }
