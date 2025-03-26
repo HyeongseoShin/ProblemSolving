ec9f405fc9101d4a3bf8d7ea8feb59766f2d8cfa
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-03-26 20:54:02 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.cc
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.cc
@@ -4,12 +4,10 @@ using namespace std;
 
 int n, k;
 
-vector<int> A; // 컨베이어 벨트
-vector<int> robotPos; // 로봇 위치
-vector<bool> isRobot; // 로봇이 있는지 없는지
+int status[250]; // 내구도 현황
+bool isRobot[250]; // 로봇이 있는지 없는지
 
-int ans = 1;
-int zeroCnt = 0;
+vector<int> robot; // 로봇의 위치 저장
 
 int main()
 {
@@ -18,79 +16,85 @@ int main()
 
     cin >> n >> k;
 
-    
-    for(int i = 0; i < n * 2; i++)
+    for(int i = 0; i < 2 * n; i++)
     {
-        int x;
-        cin >> x;
-        A.push_back(x);
-        isRobot.push_back(false);
+        cin >> status[i];
     }
 
-    int st = 0; // 현재 올리는 위치
-    int en = n - 1; // 현재 내리는 위치
-    int zCnt = 0; // 내구도 0인 칸 개수
-    int ans = 1; // 현재 단계
+    int ans = 0;
+    int cnt = 1;
+
+    int st = 0;
+    int en = n - 1;
 
     while(true)
     {
-        // 1. 벨트가 각 칸 위에 있는 로봇과 함께 한 칸 회전한다.
+        // 1. 벨트 & 로봇 한 칸 회전
         st--;
-        en--;
-        if(st < 0) st = n * 2 - 1;
-        if(en < 0) en = n * 2 - 1;
+        if(st < 0) st = 2 * n - 1;
 
-        // * 언제든지 로봇이 내리는 위치에 도달하면 그 즉시 내린다.
+        en--;
+        if(en < 0) en = 2 * n - 1;
+
+        // 언제나 로봇이 내리는 위치에 도착하면 내림
         if(isRobot[en])
         {
             isRobot[en] = false;
-            robotPos.erase(robotPos.begin());
+            
+            robot.erase(robot.begin());
         }
 
-        // 2. 가장 먼저 벨트에 올라간 로봇부터, 벨트가 회전하는 방향으로 한 칸 이동할 수 있다면 이동한다. 만약 이동할 수 없다면 가만히 있는다.
-        for(int i = 0; i < (int)robotPos.size(); i++)
+        // 2. 가장 앞에 있는 로봇부터 한 칸 이동
+        for(int i = 0; i < (int)robot.size(); i++)
         {
-            int curPos = robotPos[i];
-            int nPos = (curPos + 1) % (n * 2);
-            
-            // 2-1. 로봇이 이동하기 위해서는 이동하려는 칸에 로봇이 없으며, 그 칸의 내구도가 1 이상 남아 있어야 한다.
-            if(isRobot[nPos] || A[nPos] <= 0) continue;
-            
-            isRobot[curPos] = false;
+            int cur = robot[i];
+            int nxt = (cur + 1) % (2 * n);
 
-            isRobot[nPos] = true;
-            robotPos[i] = nPos;
-            A[nPos]--;
+            // 다음 위치에 로봇이 있으면 Pass
+            if(isRobot[nxt]) continue;
 
-            
-            if(A[nPos] == 0) zCnt++;
+            // 내구도가 1보다 작으면 Pass
+            if(status[nxt] < 1) continue;
 
-            // * 언제든지 로봇이 내리는 위치에 도달하면 그 즉시 내린다.
-            if(nPos == en)
+            // 로봇 없으면 이동
+            isRobot[cur] = false;
+            robot[i] = nxt;
+            isRobot[nxt] = true;
+
+            // 내구도 감소
+            status[nxt]--;
+
+            // 내구도 0이면 ++
+            if(status[nxt] == 0) ans++;
+
+            // 로봇이 내리는 칸에 도착하면 로봇 내리기
+            if(nxt == en)
             {
-                isRobot[nPos] = false;
-                robotPos.erase(robotPos.begin() + i);
+                isRobot[nxt] = false;
+
+                // 로봇 삭제
+                robot.erase(robot.begin());
                 i--;
             }
         }
 
-        // 3. 올리는 위치에 있는 칸의 내구도가 0이 아니면 올리는 위치에 로봇을 올린다.
-        if(A[st] > 0)
+        // 3. 올리는 위치 내구도 0 아니면 올리기
+        if(status[st] > 0)
         {
             isRobot[st] = true;
-            robotPos.push_back(st);
-            A[st]--;
+            robot.push_back(st);
+            status[st]--;
 
-            if(A[st] == 0) zCnt++;
+            if(status[st] == 0) ans++;
         }
 
-        // 4. 내구도가 0인 칸의 개수가 K개 이상이라면 과정을 종료한다. 그렇지 않다면 1번으로 돌아간다.
-        if(zCnt >= k) break;
-
-        ans++; // 단계 증가
+        if(ans >= k) break;
+        cnt++;
     }
 
-    cout << ans << "\n";
+    cout << cnt << "\n";
+
+
 
     return 0;
 }
