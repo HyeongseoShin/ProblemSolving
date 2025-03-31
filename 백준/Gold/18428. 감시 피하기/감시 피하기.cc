f5ccd7870197f8b7d62c878199e132cd7439d335
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-03-31 22:58:53 +0900

    [Gold V] Title: 감시 피하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.cc
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+char board[10][10];
+
+vector<pair<int, int>> sPos;
+vector<pair<int, int>> tPos;
+vector<pair<int, int>> oPos;
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+
+bool isPossible = false;
+
+// 선생이 학생 볼 수 있는지 검사
+bool Check()
+{
+    for(int k = 0; k < (int)tPos.size(); k++)
+    {
+        auto [curX, curY] = tPos[k];
+
+        for(int dir = 0; dir < 4; dir++)
+        {
+            int nX = curX;
+            int nY = curY;
+
+            while(nX >= 0 && nX < n && nY >= 0 && nY < n)
+            {
+                nX = nX + dx[dir];
+                nY = nY + dy[dir];
+
+                if(board[nX][nY] == 'O') break;
+                if(board[nX][nY] == 'S') return false;
+            }
+        }
+    }
+    
+
+    return true;
+}
+
+void SetObstacles(int idx, int cnt)
+{
+    // 장애물 3개 설치했으면 끝
+    if(cnt == 3)
+    {        
+        // 검사
+        isPossible = Check();
+        return;
+    }
+
+    for(int k = idx; k < (int)oPos.size(); k++)
+    {
+        if(isPossible) continue;
+
+        auto [curX, curY] = oPos[k];
+
+        board[curX][curY] = 'O';
+
+        SetObstacles(k + 1, cnt + 1);
+
+        board[curX][curY] = 'X';
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cin >> board[i][j];
+
+            // if(board[i][j] == 'S') sPos.push_back({i, j});
+            if(board[i][j] == 'X') oPos.push_back({i, j});
+            if(board[i][j] == 'T') tPos.push_back({i, j});
+        }
+    }
+
+    // // 장애물 설치 위치 후보 리스트 저장
+    // for(int i = 0; i < (int)sPos.size(); i++)
+    // {
+    //     auto [curX, curY] = sPos[i];
+
+    //     for(int dir = 0; dir < 4; dir++)
+    //     {
+    //         int nX = curX + dx[dir];
+    //         int nY = curY + dy[dir];
+
+    //         if(nX < 0 || nX >= n || nY < 0 || nY >= n) continue;
+    //         if(board[nX][nY] != 'X') continue;
+
+    //         oPos.push_back({nX, nY});
+    //     }
+    // }
+
+    // oPos.erase(unique(oPos.begin(), oPos.end()), oPos.end());
+    SetObstacles(0, 0);
+
+    if(isPossible) cout << "YES\n";
+    else cout << "NO\n";
+
+    return 0;
+
+}
