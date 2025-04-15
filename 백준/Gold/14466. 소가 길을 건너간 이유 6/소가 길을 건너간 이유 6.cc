9e07d8af7f8d3027ab8111b8b112e662b219d776
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-04-15 20:04:21 +0900

    [Gold III] Title: 소가 길을 건너간 이유 6, Time: 12 ms, Memory: 2120 KB -BaekjoonHub

--- 백준/Gold/14466. 소가 길을 건너간 이유 6/소가 길을 건너간 이유 6.cc
+++ 백준/Gold/14466. 소가 길을 건너간 이유 6/소가 길을 건너간 이유 6.cc
@@ -4,24 +4,28 @@ using namespace std;
 
 int n, k, r;
 
-// bridge[i][j][n] : (i,j) 의 n방향에 다리가 있음
+// 다리 표시
+int bridge[105][105];
 
-int bridge[103][103][4];
-int cow [103][103];
+// 각 칸에 소 위치
+int cowPos[105][105];
 
-int dx[4] = {-1, 1, 0, 0};
-int dy[4] = {0, 0, -1, 1};
+// 방문 확인
+bool vis[105][105];
 
-bool vis[103][103];
+// 소 위치
+vector<pair<int, int>> cows;
+
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
 
 int ans = 0;
 
 void BFS(int x, int y)
 {
-    vis[x][y] = true;
-
     queue<pair<int, int>> q;
     q.push({x, y});
+    vis[x][y] = true;
 
     while(!q.empty())
     {
@@ -32,36 +36,26 @@ void BFS(int x, int y)
 
         for(int i = 0; i < 4; i++)
         {
+            // cout << "bridge: " << bridge[curX][curY] << "\n";
+            if((bridge[curX][curY] >> i) & 1) continue;
+            
             int nX = curX + dx[i];
             int nY = curY + dy[i];
 
             if(nX < 1 || nX > n || nY < 1 || nY > n) continue;
             if(vis[nX][nY]) continue;
-            if(bridge[curX][curY][i] == 1) continue;
+            if(cowPos[nX][nY] != 0)
+            {
+                ans++;
+                // cout << "출발 : " << cowPos[x][y] << " 도착: " << cowPos[nX][nY] << "\n";
+            }
 
             vis[nX][nY] = true;
             q.push({nX, nY});
-
-            if(cow[nX][nY] == 1)
-            {
-                // cout << x << " " << y << " " << nX << " " << nY << "\n"; 
-                ans++;
-            }
-        }
-    }
-    
-}
-
-void Init()
-{
-    for(int i = 1; i <= n; i++)
-    {
-        for(int j = 1; j <= n; j++)
-        {
-            vis[i][j] = false;
         }
     }
 }
+
 int main()
 {
     ios::sync_with_stdio(0);
@@ -77,63 +71,34 @@ int main()
 
         for(int j = 0; j < 4; j++)
         {
-            int nX = r1 + dx[j];
-            int nY = c1 + dy[j];
-
-            if(nX == r2 && nY == c2)
+            if((r2 - r1) == dx[j] && (c2 - c1) == dy[j])
             {
-                bridge[r1][c1][j] = 1;
-                
-                // 연결된 다리 표시
-                if(j % 2 == 0) bridge[r2][c2][j+1] = 1;
-                else bridge[r2][c2][j-1] = 1;
+                bridge[r1][c1] += (1 << j);
+
+                if(j % 2 == 0) bridge[r2][c2] += (1 << (j+1));
+                else bridge[r2][c2] += (1 << (j-1));
             }
         }
-
-        
-
     }
 
     for(int i = 0; i < k; i++)
     {
         int x, y;
-
         cin >> x >> y;
-
-        cow[x][y] = 1;
-
-    }
-
-    for(int i = 1; i <= n; i++)
-    {
-        for(int j = 1; j <= n; j++)
-        {
-            if(cow[i][j] == 1)
-            {
-                // cout << "start: " << " x : " << i << " y : " << j << "\n";
-                Init();
-                BFS(i, j);
-                
-            }
-        }
-
-        // cout << "\n=====================\n";
-        // for(int x = 1; x <= n; x++)
-        // {
-        //     for(int y = 1; y <= n; y++)
-        //     {
-        //         cout << vis[x][y] << " ";
-        //     }
-        //     cout << "\n";
-        // }
         
+        cows.push_back({x, y});
+        cowPos[x][y] = (i+1);
     }
 
-    ans = (k * (k-1) - ans) / 2;
+    for(int i = 0; i < k; i++)
+    {
+        auto [x, y] = cows[i];
+        
+        memset(vis, false, sizeof(vis));
+        BFS(x, y);
+    }
 
-    cout << ans << "\n";
-
-    
+    cout << ((k * (k-1)) - ans) / 2 << "\n";
 
     return 0;
 }
