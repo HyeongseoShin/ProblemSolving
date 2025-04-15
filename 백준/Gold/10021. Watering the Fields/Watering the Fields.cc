6ed7471598f86d4add2c380f299086ab896cf7ed
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-04-15 20:34:43 +0900

    [Gold III] Title: Watering the Fields, Time: 100 ms, Memory: 39056 KB -BaekjoonHub

--- 백준/Gold/10021. Watering the Fields/Watering the Fields.cc
+++ 백준/Gold/10021. Watering the Fields/Watering the Fields.cc
@@ -2,24 +2,22 @@
 
 using namespace std;
 
-typedef long long ll;
-
-#define MAX 3000001
-
 int n, c;
 
-// (x, y)
-vector<pair<int, int>> farm;
-
-// 거리 오름차순 정렬
-priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
+vector<pair<int, int>> edges;
+vector<pair<int, int>> fields;
 
 int parent[2005];
 
+priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
 
 int Find(int v)
 {
-    if(v == parent[v]) return parent[v];
+    if(parent[v] == v)
+    {
+        return parent[v];
+    }
+    
     return parent[v] = Find(parent[v]);
 }
 
@@ -30,6 +28,11 @@ void Union(int v1, int v2)
 
     if(v1 < v2) parent[v2] = v1;
     else parent[v1] = v2;
+}
+
+int GetDist(int x1, int x2, int y1, int y2)
+{
+    return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
 }
 
 int main()
@@ -43,44 +46,45 @@ int main()
     {
         int x, y;
         cin >> x >> y;
-        farm.push_back({x, y});
 
         parent[i] = i;
+        fields.push_back({x, y});
     }
 
-    for(int i = 0; i < farm.size(); i++)
-    {   
-        for(int j = i+1; j < farm.size(); j++)
-        {
-            int dist = (farm[i].first - farm[j].first) * (farm[i].first - farm[j].first)
-            + (farm[i].second - farm[j].second) * (farm[i].second - farm[j].second);
+    for(int i = 0; i < n; i++)
+    {
+        auto [r1, c1] = fields[i];
 
+        for(int j = i + 1; j < n; j++)
+        {
+            auto [r2, c2] = fields[j];
+
+            int dist = GetDist(r1, r2, c1, c2);
             if(dist < c) continue;
+
             pq.push({dist, i, j});
         }
     }
 
     int cnt = 0;
     int ans = 0;
-    // MST의 크기가 n-1이 될때까지 진행
-    while(!pq.empty() && cnt < n - 1)
+
+    while(cnt < n - 1 && !pq.empty())
     {
-        auto [curEdge, curU, curV] = pq.top();
+        auto [w, u, v] = pq.top();
         pq.pop();
 
-        if(Find(curU) != Find(curV))
+        
+        if(Find(u) != Find(v))
         {
-            Union(curU, curV);
+            Union(u, v);
             cnt++;
-            ans += curEdge;
+            ans += w;
         }
     }
 
-    if(cnt < n - 1)
-    {
-        cout << "-1\n";
-    }
-    else cout << ans << "\n";
+    if(cnt == n - 1) cout << ans << "\n";
+    else cout << "-1\n";
     
     return 0;
 }
