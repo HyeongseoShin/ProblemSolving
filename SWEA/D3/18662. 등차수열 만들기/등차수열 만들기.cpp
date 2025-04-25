0a4592f85029c56e1d5df5dae8d04b6f89247460
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-04-26 01:11:35 +0900

    [D3] Title: 등차수열 만들기, Time: 4 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D3/18662. 등차수열 만들기/등차수열 만들기.cpp
+++ SWEA/D3/18662. 등차수열 만들기/등차수열 만들기.cpp
@@ -16,46 +16,20 @@ int main()
         
         cin >> a >> b >> c;
 
-        double ans;
+
         
-        cout << "#" << t << " ";
-        if(b - a == c - b)
-        {
-        	cout << "0.0\n";
-            continue;
-        }
-
-
-        else if(a <= b && b <= c)
-        {
-
-            double tmp1 = abs((double)(c + a) / 2 - b);
-            double tmp2 = abs(b - (double)(c - b) - a);
-            double tmp3 = abs(b + (double)(b - a) - c);
+        
+        double ans;       
+        double tmp1 = abs((double)(c + a) / 2 - b);
+        double tmp2 = abs(b - (double)(c - b) - a);
+        double tmp3 = abs(b + (double)(b - a) - c);
             
-            ans = min(min(tmp1, tmp2), tmp3);
-        }
-        
-        else if(a > b)
-        {
-            double tmp1 = abs((double)b - (c - b) - a);
-            double tmp2 = abs((double)(c + a) / 2 - b);
-            
-            ans = min(tmp1, tmp2);
-        }
-        
-        else if(b > c)
-        {
-            double tmp1 = abs((double)b + (b - a) - c);
-            double tmp2 = abs((double)(c + a) / 2 - b );
-                        
-            ans = min(tmp1, tmp2);
-        }
+        ans = min(min(tmp1, tmp2), tmp3);
         
         cout << fixed;
         cout.precision(1);
         
-        cout << ans << "\n";
+        cout << "#" << t << " " << ans << "\n";
     }
     return 0;
 }
