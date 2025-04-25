90c61a307062797ace7a9497a59b1c888ea36510
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-04-25 22:35:38 +0900

    [D3] Title: 팰린드롬 문제, Time: 151 ms, Memory: 6,000 KB -BaekjoonHub

---
+++ SWEA/D3/19003. 팰린드롬 문제/팰린드롬 문제.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int test;
+    cin >> test;
+
+    for(int t = 1; t <= test; t++)
+    {
+        int n, m;
+        cin >> n >> m;
+        
+        int cnt = 0;
+        
+        vector<string> v;
+        
+        for(int i = 0; i < n; i++)
+        {
+            string s;
+            cin >> s;
+            
+            if(s != string(s.rbegin(), s.rend()))	v.push_back(s);
+            else cnt = 1;
+        }
+        
+        if(!v.empty())
+        {
+            for(int i = 0; i < (int)v.size() - 1; i++)
+            {
+                if(v[i] == "") continue;
+                for(int j = i + 1; j < (int)v.size(); j++)
+                {
+                    if(v[j] == "") continue;
+                    if(v[i] == string(v[j].rbegin(), v[j].rend()))
+                    {
+                        v[i] = "";
+                        v[j] = "";
+                        cnt += 2;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        
+                   
+         cout << "#" << t << " " << cnt * m << "\n";
+    }
+    return 0;
+}
