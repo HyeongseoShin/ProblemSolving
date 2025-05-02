e61c482fa93922b13203ccb7d9657d42dc20961c
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-02 10:11:21 +0900

    [level 2] Title: 전화번호 목록, Time: 76.46 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -3,18 +3,24 @@
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    bool answer = true;
+    sort(phone_book.begin(), phone_book.end());
     
-    unordered_set<string> us(phone_book.begin(), phone_book.end());
-    
-    for(auto s : phone_book)
+    for(int i = 0; i < (int)phone_book.size() - 1; i++)
     {
-        string tmp = "";
-        for(int i = 0; i < (int)s.length() - 1; i++)
-        {
-            tmp += s[i];
-            if(us.count(tmp)) return false;
-        }
+        if(phone_book[i+1].substr(0, phone_book[i].length()) == phone_book[i]) return false;
     }
+    
     return true;
+//     unordered_set<string> us(phone_book.begin(), phone_book.end());
+    
+//     for(auto s : phone_book)
+//     {
+//         string tmp = "";
+//         for(int i = 0; i < (int)s.length() - 1; i++)
+//         {
+//             tmp += s[i];
+//             if(us.count(tmp)) return false;
+//         }
+//     }
+//     return true;
 }
