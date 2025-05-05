1018ec39135697d79aac25c068f289d91523d05b
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-05 17:26:03 +0900

    [Silver II] Title: 아기 상어 2, Time: 0 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.cc
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.cc
@@ -11,38 +11,32 @@ int dy[8] = {-1, 0, 1, 1, 1, 0, -1, -1};
 
 int dist[50][50];
 
-bool vis[50][50];
-
 int ans = 0;
 
-void BFS(int x, int y)
+queue<pair<int, int>> q;
+
+void BFS()
 {
-    queue<tuple<int, int, int>> q;
-
-    q.push({x, y, 0});
-
-    vis[x][y] = true;
-
     while(!q.empty())
     {
-        auto [curX, curY, distance] = q.front();
+        auto [curX, curY] = q.front();
         q.pop();
-
-        if(shark[curX][curY] == 1)
-        {
-            dist[x][y] = min(dist[x][y], distance);
-        }
-
+        
         for(int i = 0; i < 8; i++)
         {
             int nX = curX + dx[i];
             int nY = curY + dy[i];
 
             if(nX < 0 || nX >= n || nY < 0 || nY >= m) continue;
-            if(vis[nX][nY]) continue;
+            
+            if(dist[nX][nY] > dist[curX][curY] + 1)
+            {
+                dist[nX][nY] = dist[curX][curY] + 1;
+                ans = max(ans, dist[nX][nY]);
+                q.push({nX, nY});
+            }
 
-            vis[nX][nY] = true;
-            q.push({nX, nY, distance + 1});
+            
         }
     }
 }
@@ -59,28 +53,17 @@ int main()
         for(int j = 0; j < m; j++)
         {
             cin >> shark[i][j];
-            dist[i][j] = 100;
+
+            dist[i][j] = INT_MAX;
+            if(shark[i][j] == 1)
+            {
+                q.push({i, j});
+                dist[i][j] = 0;
+            }
         }
     }
 
-    
-
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < m; j++)
-        {
-            memset(vis, false, sizeof(vis));
-            BFS(i, j);
-        }
-    }
-
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < m; j++)
-        {
-            ans = max(ans, dist[i][j]);
-        }
-    }
+    BFS();
 
     cout << ans << "\n";
 
