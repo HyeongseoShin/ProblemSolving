2e8bd1f6d20f48aa26dc3f05d22821e9f66d5613
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-06 12:17:26 +0900

    [level 2] Title: 큰 수 만들기, Time: 2243.16 ms, Memory: 5.4 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -7,11 +7,14 @@ string solution(string number, int k) {
     int n = (int)number.length() - k;
     
     int idx = 0;
+    
     // 앞에서부터 k개씩 보며 그 중 가장 큰 수 선택
     for(int i = 0; i < n; i++)
     {
+        // 현재 범위에서 가장 큰 수
         int maxIdx = idx;
         
+        // 이전 가장 큰 숫자 ~ (k + i) 까지 검사하며 가장 큰 수 뽑기
         for(int j = idx; j <= k + i; j++)
         {
             if(number[maxIdx] < number[j])
