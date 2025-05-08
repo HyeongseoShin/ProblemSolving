2dc79a5d98da828c9c12e0537ce127927bcba796
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-08 16:28:13 +0900

    [level 2] Title: 구명보트, Time: 1.82 ms, Memory: 4.79 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -3,22 +3,22 @@
 using namespace std;
 
 int solution(vector<int> people, int limit) {
-    
-    int ans = 0;
-    int n = (int)people.size();
-    
-    sort(people.begin(), people.end());
+    int answer = 0;
     
     int st = 0;
-    int en = n - 1;
+    int en = (int)people.size() - 1;
     
+    sort(people.begin(), people.end());
     while(st <= en)
     {
-        if(people[st] + people[en] <= limit) st++;
-        en--;
+        if(people[st] + people[en] <= limit)
+        {
+            st++;
+        }
         
-        ans++;
+        en--;
+        answer++;
+        
     }
-    
-    return ans;
+    return answer;
 }
