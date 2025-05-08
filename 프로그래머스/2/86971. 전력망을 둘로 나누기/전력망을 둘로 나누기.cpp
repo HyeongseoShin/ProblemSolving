e15c3b1963029c79197b2d005118bb623794bb49
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-08 17:30:53 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.28 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -2,79 +2,65 @@
 
 using namespace std;
 
-set<int> adj[101];
+int graph[101][101];
 
 bool vis[101];
 
-vector<int> amount;
-
-int ans = 100;
-
-int cur = 0;
-
-void BFS(int idx)
-{    
-    int cnt = 0;
-    
-    queue<int> q;
-    q.push(idx);
-    
-    vis[idx] = true;
-    
-    while(!q.empty())
+int sz;
+void DFS(int x)
+{
+    for(int i = 1; i <= sz; i++)
     {
-        int curIdx = q.front();
-        q.pop();
+        if(graph[x][i] == 0) continue;
+        if(vis[i]) continue;
         
-        cnt++;
-        for(auto nxt : adj[curIdx])
-        {
-            if(vis[nxt]) continue;
-            
-            vis[nxt] = true;
-            q.push(nxt);
-        }
+        vis[i] = true;
+        DFS(i);
     }
-    
-    cur = max(cur, cnt);
 }
 
-int solution(int n, vector<vector<int>> wires) {    
-    for(int i = 0; i < n - 1; i++)
+int solution(int n, vector<vector<int>> wires) {
+    int answer = INT_MAX;
+    sz = n;
+    int m = (int)wires.size();
+    
+    // 연결
+    for(int i = 0; i < m; i++)
     {
         int from = wires[i][0];
         int to = wires[i][1];
         
-        adj[from].insert(to);
-        adj[to].insert(from);
+        graph[from][to] = 1;
+        graph[to][from] = 1;
     }
     
-    for(int i = 0; i < n - 1; i++)
+    for(int i = 0; i < m; i++)
     {
-        fill(vis, vis + 101, false);
-        amount.clear();
-            
         int from = wires[i][0];
         int to = wires[i][1];
+      
+        // 연결 끊기
+        graph[from][to] = 0;
+        graph[to][from] = 0;
         
-        adj[from].erase(to);
-        adj[to].erase(from);
-                
-        for(int j = 1; j <= n; j++)
+        fill(vis, vis + 101, false);        
+        vis[1] = true;
+        
+        DFS(1);
+        
+        // 다시 연결
+        graph[from][to] = 1;
+        graph[to][from] = 1;
+        
+        int cnt = 0;
+        for(int i = 1; i <= n; i++)
         {
-            if(vis[j]) continue;
-            
-            cur = 0;
-            
-            BFS(j);    
-            amount.push_back(cur);
+            if(vis[i]) cnt++;
         }
         
-        ans = min(ans, abs(amount[0] - amount[1]));
-        
-        adj[from].insert(to);
-        adj[to].insert(from);
+        cout << "n: " << n << " cnt: " << cnt << "\n";
+        answer = min(answer, abs((n - cnt) - cnt));
     }
     
-    return ans;
+    return answer;
 }
