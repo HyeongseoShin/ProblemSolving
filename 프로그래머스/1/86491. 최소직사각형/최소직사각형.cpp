b299bd60d48f9fd5de186010f21b7b661606738d
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-08 13:53:56 +0900

    [level 1] Title: 최소직사각형, Time: 0.66 ms, Memory: 5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -3,37 +3,16 @@
 using namespace std;
 
 int solution(vector<vector<int>> sizes) {
-    int w = 0, h = 0;
-    
+    int w = 0, h = 0;    
     int n = (int)sizes.size();
     
+    // 가로가 더 큰 값이 되도록
     for(int i = 0; i < n; i++)
     {
-        w = max(w, sizes[i][0]);
-        h = max(h, sizes[i][1]);
+        w = max(w, max(sizes[i][0], sizes[i][1]));
+        h = max(h, min(sizes[i][0], sizes[i][1]));
     }
     
-    int maxVal = max(w, h);
-    int minVal = min(w, h);
+    return w * h;
     
-    // cout << "maxVal: " << maxVal << " minVal: " << minVal << "\n";
-    while(true)
-    {
-        minVal--;        
-        bool isPossible = true;
-        
-        for(int i = 0; i < n; i++)
-        {
-            if((sizes[i][0] <= minVal && sizes[i][1] <= maxVal) || (sizes[i][0] <= maxVal && sizes[i][1] <= minVal)) continue;
-            
-            isPossible = false;
-            break;
-        }
-        
-        if(!isPossible) break;
-        
-        
-    }
-    
-    return maxVal * (minVal + 1);
 }
