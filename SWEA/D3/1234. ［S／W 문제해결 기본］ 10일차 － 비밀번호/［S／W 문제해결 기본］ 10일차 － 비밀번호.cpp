c788cfd707544e4c0774b0f24f2c794d1292f2f6
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-12 15:38:57 +0900

    [D3] Title: [S/W 문제해결 기본] 10일차 - 비밀번호, Time: 4 ms, Memory: 408 KB -BaekjoonHub

--- SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.cpp
+++ SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.cpp
@@ -15,45 +15,24 @@ int main()
         string s;
 		cin >> s;
         
-        int st = 0;
-        int en = 1;
+        stack<char> stk;
         
-        bool isRemoved[100];
-        fill(isRemoved, isRemoved + 100, false);
-        
-        while(en < n && st < en)
-        {
-            if(s[st] != s[en])
-            {
-                st++;
-                en++;
-            }
-            
-            while(st >= 0 && en < n && s[st] == s[en])
-            {
-                isRemoved[st] = true;
-                isRemoved[en] = true;
-                
-                while(isRemoved[st]) st--;
-                while(isRemoved[en]) en++;
-            }
-            
-            // cout << "st: " << st <<" en: " << en << "\n";
-            
-            st = en;
-            en++;
-            
-
-        }
-        
-        string ans = "";
         for(int i = 0; i < n; i++)
         {
-            if(!isRemoved[i]) ans += s[i];
+            if(!stk.empty() && stk.top() == s[i]) stk.pop();
+            else stk.push(s[i]);
         }
         
-        cout << "#" << t << " " << ans << "\n";
+        string tmp = "";
         
+        while(!stk.empty())
+        {
+            tmp += stk.top();
+            stk.pop();
+        }
+        
+        reverse(tmp.begin(), tmp.end());
+        cout << "#" << t << " " << tmp << "\n";
     }
     return 0;
 }
