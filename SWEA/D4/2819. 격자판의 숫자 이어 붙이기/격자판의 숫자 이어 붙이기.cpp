cc8c02a68aba712a3d09426973dbb88225197319
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-12 18:08:45 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 186 ms, Memory: 7,180 KB -BaekjoonHub

---
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.cpp
@@ -0,0 +1,65 @@
+// https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=4&contestProbId=AV7I5fgqEogDFAXB&categoryId=AV7I5fgqEogDFAXB&categoryType=CODE&problemTitle=&orderBy=RECOMMEND_COUNT&selectCodeLang=ALL&select-1=4&pageSize=10&pageIndex=1
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+
+char board[4][4];
+
+set<string> s;
+
+void GetAns(int x, int y, string cur, int cnt)
+{
+    if(cnt == 6)
+    {
+        s.insert(cur);
+        return;
+    }
+
+    for(int i = 0; i < 4; i++)
+    {
+        int nX = x + dx[i];
+        int nY = y + dy[i];
+
+        if(nX < 0 || nX >= 4 || nY < 0 || nY >= 4) continue;
+        GetAns(nX, nY, cur + board[nX][nY], cnt + 1);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int testCase;
+    cin >> testCase;
+
+    for(int t = 1; t <= testCase; t++)
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+                cin >> board[i][j];
+            }
+        }
+
+        s.clear();
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+                string tmp = "";
+                tmp += board[i][j];
+                GetAns(i, j, tmp, 0);
+            }
+        }
+
+        cout << "#" << t << " " << (int)s.size() << "\n";
+    }
+
+    return 0;
+}
