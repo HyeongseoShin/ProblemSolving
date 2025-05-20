33c4d263aaf54d9680b4ef2c9431a73438205575
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-20 11:30:14 +0900

    [D3] Title: 의석이의 세로로 말해요, Time: 4 ms, Memory: 400 KB -BaekjoonHub

--- SWEA/D3/5356. 의석이의 세로로 말해요/의석이의 세로로 말해요.cpp
+++ SWEA/D3/5356. 의석이의 세로로 말해요/의석이의 세로로 말해요.cpp
@@ -1,50 +1,40 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
+ 
     int testCase;
     cin >> testCase;
-
+ 
     for(int t = 1; t <= testCase; t++)
     {
-        char board[5][15];
-
-        for(int i = 0; i < 5; i++)
-        {
-            for(int j = 0; j < 15; j++)
-            {
-                board[i][j] = '!';
-            }
-        }
-
+        vector<string> v;
+ 
         for(int i = 0; i < 5; i++)
         {
             string s;
             cin >> s;
-            
-            for(int j = 0; j < (int)s.length(); j++)
-            {
-                board[i][j] = s[j];
-            }
+            v.push_back(s);
+             
         }
-
+ 
         string ans = "";
+ 
         for(int i = 0; i < 15; i++)
         {
             for(int j = 0; j < 5; j++)
             {
-                if(board[j][i] == '!') continue;
-                ans += board[j][i];
+                if((int)v[j].size() <= i) continue;
+                ans += v[j][i];
             }
         }
-
+ 
         cout << "#" << t << " " << ans << "\n";
     }
-
+ 
     return 0;
 }
