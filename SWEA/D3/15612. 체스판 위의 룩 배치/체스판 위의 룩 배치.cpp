0fbc68bde068cf95c12668c60ddef4af3cf17ef8
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-21 09:46:43 +0900

    [D3] Title: 체스판 위의 룩 배치, Time: 4 ms, Memory: 396 KB -BaekjoonHub

---
+++ SWEA/D3/15612. 체스판 위의 룩 배치/체스판 위의 룩 배치.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int testCase;
+    cin >> testCase;
+
+    for(int t = 1; t <= testCase; t++)
+    {
+        int board[8][8];
+
+        int cnt = 0;
+        for(int i = 0; i < 8; i++)
+        {
+            string s;
+            cin >> s;
+
+            
+            for(int j = 0; j < 8; j++)
+            {
+                board[i][j] = s[j];
+
+                if(board[i][j] == 'O') cnt++;
+            }
+        }
+
+        cout << "#" << t << " ";
+        if(cnt != 8)
+        {
+            cout << "no\n";
+            continue;
+        }
+
+        bool row[8];
+        bool col[8];
+
+        fill(row, row + 8, false);
+        fill(col, col + 8, false);
+
+        bool isPossible = true;
+        for(int i = 0; i < 8; i++)
+        {
+            for(int j = 0; j < 8; j++)
+            {
+                if(board[i][j] == 'O')
+                {
+                    if(row[i] || col[j])
+                    {
+                        isPossible = false;
+                        break;
+                    }
+
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+
+            if(!isPossible) break;
+        }
+
+        if(isPossible) cout << "yes\n";
+        else cout << "no\n";
+
+
+    }
+    return 0;
+}
