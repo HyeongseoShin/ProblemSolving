4a1bf23c7ddc13bc45d2874c5d529fd5708924ee
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-21 11:57:40 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 2,676 ms, Memory: 5,988 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.cpp
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.cpp
@@ -2,59 +2,55 @@
 
 using namespace std;
 
-int p1[9];
-bool isUsed[19];
-int p2[9];
-
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     int testCase;
     cin >> testCase;
-    
+
     for(int t = 1; t <= testCase; t++)
     {
-        fill(isUsed, isUsed + 19, false);
+        bool cards[19];
+        fill(cards, cards + 19, false);
+
+        int p1[9];
         for(int i = 0; i < 9; i++)
         {
             cin >> p1[i];
-            isUsed[p1[i]] = true;
+            cards[p1[i]] = true;
         }
-        
+
+        int p2[9];
         int idx = 0;
         for(int i = 1; i <= 18; i++)
         {
-            if(!isUsed[i])
-            {
-                p2[idx++] = i;
-            }
+            if(!cards[i]) p2[idx++] = i;
         }
-        
-        int ans1 = 0;
-        int ans2 = 0;
-        
-        int p1Cnt = 0;
-        int p2Cnt = 0;
-        
+
+        int p1Win = 0;
+        int p2Win = 0;
+
         do
         {
-			p1Cnt = 0;
-          	p2Cnt = 0;
-            
-			for(int i = 0; i < 9; i++)
+            int p1Score = 0;
+            int p2Score = 0;
+
+            for(int i = 0; i < 9; i++)
             {
-                if(p1[i] > p2[i]) p1Cnt += (p1[i] + p2[i]);
-                else p2Cnt += (p1[i] + p2[i]);
+                if(p1[i] > p2[i]) p1Score += (p1[i] + p2[i]);
+                else p2Score += (p1[i] + p2[i]);
             }
-            
-            if(p1Cnt > p2Cnt) ans1++;
-            else if(p1Cnt < p2Cnt) ans2++;
-            
+
+            if(p1Score > p2Score) p1Win++;
+            else if(p1Score < p2Score) p2Win++;
+
         } while(next_permutation(p2, p2 + 9));
-        
-        cout << "#" << t << " " << ans1 << " " << ans2 << "\n";
+
+        cout << "#" << t << " " << p1Win << " " << p2Win << "\n";
     }
+
+
     return 0;
 }
