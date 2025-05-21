4a0d628d0f5deb11221ce9b3e8a49748b1ea20b5
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-21 14:27:40 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 17 ms, Memory: 5,992 KB -BaekjoonHub

--- SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.cpp
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.cpp
@@ -2,82 +2,84 @@
 
 using namespace std;
 
-int GetMaxLenPalindrome(string target)
+vector<string> v;
+
+int ans = 1;
+
+void GetMaxPalindrome(string s)
 {
-    int ret = 1;
-    
-    // 홀수 길이 문자의 팰린드롬 검사
+    // 홀수 길이
     for(int i = 0; i < 100; i++)
     {
         int l = i;
         int r = i;
-        
-        while(l >= 0 && r < 100 & target[l] == target[r])
+
+        while(l >= 0 && r < 100 && s[l] == s[r])
         {
-            ret = max(ret, r - l + 1);
+            ans = max(ans, r - l + 1);
+
             l--;
             r++;
         }
-        
     }
-    
-    // 짝수 길이 문자의 팰린드롬 검사
+
+    // 짝수 길이
     for(int i = 0; i < 99; i++)
     {
         int l = i;
         int r = i + 1;
-        
-        while(l >= 0 && r < 100 & target[l] == target[r])
+
+        while(l >= 0 && r < 100 && s[l] == s[r])
         {
-            ret = max(ret, r - l + 1);
+            ans = max(ans, r - l + 1);
+
             l--;
             r++;
         }
     }
-    
-    return ret;
 }
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     for(int t = 1; t <= 10; t++)
     {
         int n;
         cin >> n;
-        
-        vector<string> v;
-        
+
+        v.clear();
+
         for(int i = 0; i < 100; i++)
         {
             string s;
             cin >> s;
+
             v.push_back(s);
         }
-        
-        int ans = 1;
-        
-        // 가로 검사
+
+        ans = 1;
+        // 가로 회문 찾기
         for(auto s : v)
         {
-            ans = max(ans, GetMaxLenPalindrome(s));
+            GetMaxPalindrome(s);
         }
-        
-        // 세로 검사
-        
+
+        // 세로 회문 찾기
         for(int i = 0; i < 100; i++)
         {
-            string tmp;
+            string tmp = "";
             for(int j = 0; j < 100; j++)
             {
                 tmp += v[j][i];
             }
-            
-            ans = max(ans, GetMaxLenPalindrome(tmp));
+
+            GetMaxPalindrome(tmp);
         }
-        
+
         cout << "#" << n << " " << ans << "\n";
     }
+
     return 0;
 }
