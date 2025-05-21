b3b29eb5101c1a6c032e28346014c0d3198fd773
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-21 10:00:00 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 10 ms, Memory: 6,024 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.cpp
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.cpp
@@ -2,32 +2,32 @@
 
 using namespace std;
 
+int board[100][100];
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     for(int t = 1; t <= 10; t++)
     {
         int n;
-        
         cin >> n;
-        
-        int board[100][100];
-        
+
         for(int i = 0; i < n; i++)
         {
-        	for(int j = 0; j < n;j++)
+            for(int j = 0; j < n; j++)
             {
-        		cin >> board[i][j];        
-            }    
+                cin >> board[i][j];
+            }
         }
-        
+
         int ans = 0;
-        int prev = 0;
+
+        // 한 열에서 위에서 아래로 순회하며 N극이 있으면 표시 후 S극을 만나면 ans++
         for(int i = 0; i < n; i++)
         {
-            prev = 0;
+            int prev = 0;
             for(int j = 0; j < n; j++)
             {
                 if(board[j][i] == 1) prev = 1;
@@ -38,10 +38,10 @@ int main()
                 }
             }
         }
-        
-        
-       
+
         cout << "#" << t << " " << ans << "\n";
     }
+
+
     return 0;
 }
