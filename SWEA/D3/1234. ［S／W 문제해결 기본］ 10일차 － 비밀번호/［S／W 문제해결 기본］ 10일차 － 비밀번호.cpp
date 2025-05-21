10a16522ab5bca6593b6634146efe8eb0bd61e51
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-21 10:28:20 +0900

    [D3] Title: [S/W 문제해결 기본] 10일차 - 비밀번호, Time: 4 ms, Memory: 4 KB -BaekjoonHub

--- SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.cpp
+++ SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.cpp
@@ -6,33 +6,41 @@ int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     for(int t = 1; t <= 10; t++)
     {
         int n;
-        cin >> n;
-        
         string s;
-		cin >> s;
-        
-        stack<char> stk;
-        
+
+        cin >> n >> s;
+
+        for(int i = 0; i < n - 1; i++)
+        {
+            if(s[i] == ' ') continue;
+            if(s[i] != s[i+1]) continue;
+
+            int st = i;
+            int en = i + 1;
+
+            while(st >= 0 && en < n && s[st] == s[en])
+            {
+                s[st] = ' ';
+                s[en] = ' ';
+
+                st--;
+                en++;
+                
+                while(st > 0 && s[st] == ' ') st--;
+                while(en > 0 && s[en] == ' ') en++;
+            }
+        }
+
+        string ans = "";
         for(int i = 0; i < n; i++)
         {
-            if(!stk.empty() && stk.top() == s[i]) stk.pop();
-            else stk.push(s[i]);
+            if(s[i] != ' ') ans += s[i];
         }
-        
-        string tmp = "";
-        
-        while(!stk.empty())
-        {
-            tmp += stk.top();
-            stk.pop();
-        }
-        
-        reverse(tmp.begin(), tmp.end());
-        cout << "#" << t << " " << tmp << "\n";
+        cout << "#" << t << " " << ans << "\n";
     }
     return 0;
 }
