2507814ec9342c194f5e2306171b3f1857701cd1
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-21 11:37:18 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 4 ms, Memory: 404 KB -BaekjoonHub

--- SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.cpp
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.cpp
@@ -6,49 +6,49 @@ int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     for(int t = 1; t <= 10; t++)
     {
         int n;
-        cin>> n;
-        
-        vector<int> nums;
-        
-        for(int i = 0; i < n; i++)
+        cin >> n;
+
+        vector<int> v;
+        for(int i = 0; i < n;i++)
         {
             int x;
             cin >> x;
-            
-           nums.push_back(x);
+            v.push_back(x);
         }
         
         int m;
         cin >> m;
-        
-        for(int i = 0; i < m; i++)
+
+        while(m--)
         {
             char op;
             cin >> op;
-            
+
             int st, cnt;
             cin >> st >> cnt;
-            
+
             vector<int> tmp;
-            for(int j = 0; j < cnt; j++)
+            for(int i = 0; i < cnt; i++)
             {
-				int x;
+                int x;
                 cin >> x;
                 tmp.push_back(x);
             }
-			nums.insert(nums.begin() + st, tmp.begin(), tmp.end());
-        }            
-        
+
+            v.insert(v.begin() + st, tmp.begin(), tmp.end());
+        }
+
         cout << "#" << t << " ";
         for(int i = 0; i < 10; i++)
         {
-            cout << nums[i] << " ";
+            cout << v[i] << " ";
         }
         cout << "\n";
     }
+
     return 0;
 }
