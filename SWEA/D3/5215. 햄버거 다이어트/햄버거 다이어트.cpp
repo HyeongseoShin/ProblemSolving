2099f522ceed567f98dd18978493475895cef33e
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-21 17:29:47 +0900

    [D3] Title: 햄버거 다이어트, Time: 16 ms, Memory: 6,688 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.cpp
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.cpp
@@ -1,5 +1,3 @@
-// Knapsack Problem (DP)
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -8,40 +6,42 @@ int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     int testCase;
     cin >> testCase;
-    
+
     for(int t = 1; t <= testCase; t++)
     {
         int n, l;
-
         cin >> n >> l;
 
-        int scores[21];
+        int score[21];
         int calories[21];
-        
+
         for(int i = 1; i <= n; i++)
         {
-            cin >> scores[i] >> calories[i];
+            cin >> score[i] >> calories[i];
         }
-        
+
+        // dp[i][j] : 최대 칼로리가 j이고 i번째 재료까지 봤을 때 점수의 최대값
         int dp[21][10001];
-        
+        memset(dp, 0, sizeof(dp));
+
         for(int i = 1; i <= n; i++)
         {
             for(int j = 1; j <= l; j++)
             {
                 if(calories[i] <= j)
                 {
-                    dp[i][j] = max(dp[i-1][j], dp[i-1][j - calories[i]] + scores[i]);
+                    dp[i][j] = max(dp[i-1][j], dp[i-1][j - calories[i]] + score[i]);
                 }
-                
                 else dp[i][j] = dp[i-1][j];
             }
         }
-        
+
         cout << "#" << t << " " << dp[n][l] << "\n";
+
+
     }
     return 0;
 }
