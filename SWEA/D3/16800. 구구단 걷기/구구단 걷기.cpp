de05eecfedc5d79cdd1dc43eec47da97b066cd25
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-21 16:17:10 +0900

    [D3] Title: 구구단 걷기, Time: 335 ms, Memory: 5,988 KB -BaekjoonHub

--- SWEA/D3/16800. 구구단 걷기/구구단 걷기.cpp
+++ SWEA/D3/16800. 구구단 걷기/구구단 걷기.cpp
@@ -1,33 +1,31 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
 typedef long long ll;
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     int testCase;
     cin >> testCase;
-    
+
     for(int t = 1; t <= testCase; t++)
     {
         ll n;
-        
         cin >> n;
-        ll ans = n - 1;
-       
-        for(ll i = 2; i <= sqrt(n); i++)
+
+        ll pos = 0;
+        for(ll i = 1; i <= sqrt(n); i++)
         {
-            if(n % i == 0)
-            {
-                ans = min(ans, (i - 1) + (n / i - 1));
-            }
+            if(n % i == 0) pos = i;
         }
-        
+
+        ll ans = pos + (n / pos) - 2;
+
         cout << "#" << t << " " << ans << "\n";
-        
     }
     return 0;
 }
