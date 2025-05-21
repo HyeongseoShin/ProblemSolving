f056d9d6be1f6fd05edeece5f7ae21669bb134d7
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-21 15:57:04 +0900

    [D3] Title: 부분 수열의 합, Time: 90 ms, Memory: 5,988 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.cpp
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.cpp
@@ -3,54 +3,52 @@
 using namespace std;
 
 int n, k;
-int ans;
+int board[20];
+bool isUsed[20];
+int ans = 0;
 
-vector<int> v;
-
-void DFS(int idx, int cur)
+void GetAns(int idx, int cur)
 {
     if(cur == k)
     {
         ans++;
         return;
     }
-    
-    for(int i = idx + 1; i < n; i++)
+
+    for(int i = idx; i < n; i++)
     {
-        if(cur + v[i] <= k) DFS(i, cur + v[i]);
+        if(isUsed[i]) continue;
+        
+        isUsed[i] = true;
+        GetAns(i + 1, cur + board[i]);
+        isUsed[i] = false;
     }
+
+
 }
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     int testCase;
     cin >> testCase;
-    
+
     for(int t = 1; t <= testCase; t++)
     {
         cin >> n >> k;
-     
-        v.resize(n);
-        fill(v.begin(), v.end(), 0);
-        
+
         for(int i = 0; i < n; i++)
         {
-            cin >> v[i];
-        }
-        
-        ans = 0;
-        
-        for(int i = 0; i < n; i++)
-        {
-	        DFS(i, v[i]);            
+            cin >> board[i];
         }
 
-        
-		        
+        ans = 0;
+        fill(isUsed, isUsed + 20, false);
+        GetAns(0, 0);
+
         cout << "#" << t << " " << ans << "\n";
-        
     }
     return 0;
 }
