28189d6b1d5c877ca38d4dc11b4ba4806c9e4a7c
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-21 11:25:10 +0900

    [D3] Title: N-Queen, Time: 8 ms, Memory: 5,988 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.cpp
+++ SWEA/D3/2806. N－Queen/N－Queen.cpp
@@ -3,59 +3,61 @@
 using namespace std;
 
 int n;
-
-int col[10];
-int ld[20];
-int rd[20];
-
 int ans = 0;
 
-void GetAns(int x)
+// 행, 열에 퀸 있는지 확인
+bool row[11];
+bool col[11];
+
+// 왼쪽 대각선, 오른쪽 대각선에 퀸 있는지 확인
+bool ld[22];
+bool rd[22];
+
+void GetAns(int idx, int cnt)
 {
-    if(x == n)
+    if(cnt == n)
     {
         ans++;
-        return ;
+        return;
     }
-    
+
     for(int i = 0; i < n; i++)
     {
-        if(col[i] == 1) continue;
-        if(ld[x + i] == 1) continue;
-        if(rd[n - (i - x) - 1] == 1) continue;
+        if(col[i]) continue;
+        if(ld[idx + i]) continue;
+        if(rd[n - (i - idx)]) continue;
 
-        col[i] = 1;
-        ld[x + i] = 1;
-        rd[n - (i - x) - 1] = 1;
+        row[idx] = true;
+        col[i] = true;
+        ld[idx + i] = true;
+        rd[n - (i - idx)] = true;
 
-        GetAns(x+1);
+        GetAns(idx + 1, cnt + 1);
 
-        col[i] = 0;
-        ld[x + i] = 0;
-        rd[n - (i - x) - 1] = 0;
+        row[idx] = false;
+        col[i] = false;
+        ld[idx + i] = false;
+        rd[n - (i - idx)] = false;
     }
 }
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     int testCase;
     cin >> testCase;
-    
+
     for(int t = 1; t <= testCase; t++)
-    {        
+    {
         cin >> n;
-        
-        fill(col, col + 10, 0);
-        fill(ld, ld + 20, 0);
-        fill(rd, rd + 20, 0);
-        
+
         ans = 0;
-        
-        GetAns(0);
-        
+        GetAns(0, 0);
+
         cout << "#" << t << " " << ans << "\n";
     }
+
     return 0;
 }
