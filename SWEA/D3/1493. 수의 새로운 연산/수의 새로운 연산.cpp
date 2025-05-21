8c2af2a85e6f4f2cdc3316eacb74919b15dc0c7b
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-21 15:43:28 +0900

    [D3] Title: 수의 새로운 연산, Time: 5 ms, Memory: 312 KB -BaekjoonHub

--- SWEA/D3/1493. 수의 새로운 연산/수의 새로운 연산.cpp
+++ SWEA/D3/1493. 수의 새로운 연산/수의 새로운 연산.cpp
@@ -2,54 +2,59 @@
 
 using namespace std;
 
-int pos[301][301];
+#define MAX 301
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
-	int cur = 1;
-    for(int i = 1; i <= 300; i++)
-    {
-        pos[i][1] = cur;
-        for(int j = 2; j <= 300; j++)
-        {
-            pos[i][j] = pos[i][j - 1] + i + j - 1;
-        }
-        cur += i;
-    }
-    
+
     int testCase;
     cin >> testCase;
-    
+
+    int board[MAX][MAX];
+
+    pair<int, int> pos[10005];
+
+    for(int i = 1; i < MAX; i++)
+    {
+        if(i == 1)
+        {
+            board[i][1] = 1;
+            pos[1] = {1, 1};
+        }
+        else
+        {
+            board[i][1] = board[i-1][1] + (i - 1);
+            int num = board[i-1][1] + (i - 1);
+
+            if(num <= 10000) pos[num] = {i, 1};
+        }
+        
+        int cnt = i + 1;
+        for(int j = 2; j < MAX; j++)
+        {
+            int idx = board[i][j-1] + cnt;
+            board[i][j] = idx;
+
+            if(idx <= 10000) pos[idx] = {i, j};
+            cnt++;
+        }
+    }
+
+
     for(int t = 1; t <= testCase; t++)
     {
         int p, q;
-        cin >> p >> q;        
-        
-        int pX, pY, qX, qY;
-        
-        for(int i = 1; i <= 300; i++)
-        {
-            for(int j = 1; j <= 300; j++)
-            {
-                if(pos[i][j] == p)
-                {
-                    pX = i;
-                    pY = j;
-                }
-                
-                if(pos[i][j] == q)
-                {
-                    qX = i;
-                    qY = j;
-                }
-            }
-        }
-        
-        cout << "#" << t << " " << pos[pX + qX][pY + qY] << "\n";
-        
+        cin >> p >> q;
+
+        pair<int, int> andP = pos[p];
+        pair<int, int> andQ = pos[q];
+
+        pair<int, int> result = {andP.first + andQ.first, andP.second + andQ.second};
+
+        cout << "#" << t << " " << board[result.first][result.second] << "\n";
     }
+
     return 0;
 }
