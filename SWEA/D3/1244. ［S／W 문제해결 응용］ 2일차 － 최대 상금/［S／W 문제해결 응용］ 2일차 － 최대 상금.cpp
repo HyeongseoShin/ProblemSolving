083139609267debd66047f8339cfdf25dfddc923
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-22 12:31:49 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 50 ms, Memory: 5,988 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.cpp
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.cpp
@@ -2,24 +2,26 @@
 
 using namespace std;
 
-int ans;
 string s;
-int cnt;
 
-void GetAns(int idx, int curCnt)
+int n, k;
+
+int ans;
+
+void GetAns(int idx, int cnt)
 {
-    if(curCnt == cnt)
+    if(cnt == k)
     {
-		ans = max(ans, stoi(s));
-		return ;
+        ans = max(ans, stoi(s));
+        return;
     }
-    
-    for(int i = idx; i < (int)s.length() - 1; i++)
+
+    for(int i = idx; i < n - 1; i++)
     {
-        for(int j = i + 1; j < (int)s.length(); j++)
+        for(int j = i + 1; j < n; j++)
         {
             swap(s[i], s[j]);
-            GetAns(i, curCnt + 1);
+            GetAns(i, cnt + 1);
             swap(s[i], s[j]);
         }
     }
@@ -29,20 +31,22 @@ int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     int testCase;
     cin >> testCase;
-    
+
     for(int t = 1; t <= testCase; t++)
     {
-        cin >> s >> cnt;
-        
+        cin >> s >> k;
+
+        n = (int)s.length();
+
+        if(k > n) k = n;
         ans = 0;
-        
-        if(cnt > (int)s.length()) cnt = (int)s.length();
         GetAns(0, 0);
         
         cout << "#" << t << " " << ans << "\n";
     }
+
     return 0;
 }
