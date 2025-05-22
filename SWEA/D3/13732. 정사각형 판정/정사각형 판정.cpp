fe3032cbab079d576f683bce886cc32f4052a047
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-22 22:14:21 +0900

    [D3] Title: 정사각형 판정, Time: 4 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D3/13732. 정사각형 판정/정사각형 판정.cpp
+++ SWEA/D3/13732. 정사각형 판정/정사각형 판정.cpp
@@ -2,80 +2,73 @@
 
 using namespace std;
 
+int n;
+
+char board[20][20];
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     int testCase;
     cin >> testCase;
-    
+
     for(int t = 1; t <= testCase; t++)
     {
-        int n;
         cin >> n;
-        
-        char board[21][21];
-        
-        int cnt = 0;
-        pair<int, int> st;
-        
+
+        int minX = 21, minY = 21;
+        int maxX = 0, maxY = 0;
+
+
+        bool isPossible = false;
         for(int i = 0; i < n; i++)
         {
             string s;
             cin >> s;
-            
+
             for(int j = 0; j < n; j++)
             {
-				board[i][j] = s[j];
+                board[i][j] = s[j];
+
                 if(board[i][j] == '#')
                 {
-                    if(cnt == 0) st = {i, j};
-                    cnt++;
+                    isPossible = true;
+                    minX = min(minX, i);
+                    minY = min(minY, j);
+
+                    maxX = max(maxX, i);
+                    maxY = max(maxY, j);
                 }
             }
         }
 
-        bool isPossible = false;
-        
-        int tmp = 1;
-        while(tmp <= 20)
-        {
-			if(tmp * tmp == cnt)
-            {
-                isPossible = true;
-                break;
-            }
-            tmp++;
-        }
+        if(maxX - minX != maxY - minY) isPossible = false;
         
         if(!isPossible)
         {
             cout << "#" << t << " no\n";
             continue;
         }
-        
-        int sz = sqrt(cnt);
-        isPossible = true;
 
-        for(int i = st.first; i < st.first + sz; i++)
+        for(int i = minX; i <= maxX; i++)
         {
-            for(int j = st.second; j < st.second + sz; j++)
+            for(int j = minY; j <= maxY; j++)
             {
-                if(board[i][j] == '.')
+                if(board[i][j] != '#')
                 {
                     isPossible = false;
                     break;
                 }
             }
-			
-			if(!isPossible) break;
+
+            if(!isPossible) break;
         }
-        
+
         cout << "#" << t << " ";
         if(isPossible) cout << "yes\n";
         else cout << "no\n";
-        
     }
     return 0;
 }
