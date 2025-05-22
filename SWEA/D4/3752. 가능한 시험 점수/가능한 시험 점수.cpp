88515ea45220a54368d9e899787c8698032eae8b
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-22 14:30:58 +0900

    [D4] Title: 가능한 시험 점수, Time: 40 ms, Memory: 5,992 KB -BaekjoonHub

--- SWEA/D4/3752. 가능한 시험 점수/가능한 시험 점수.cpp
+++ SWEA/D4/3752. 가능한 시험 점수/가능한 시험 점수.cpp
@@ -1,5 +1,3 @@
-// 백트래킹 2^ 100 이라서 시간 초과
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -8,51 +6,44 @@ int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     int testCase;
     cin >> testCase;
-    
+
     for(int t = 1; t <= testCase; t++)
     {
         int n;
         cin >> n;
 
-        // 입력받은 점수 목록
-        vector<int> scores;
+        int score[101];
         
         for(int i = 0; i < n; i++)
         {
-            int x;
-            cin >> x;
-            scores.push_back(x);
+            cin >> score[i];
         }
 
-         // 가능한 점수 목록
-        vector<int> v;
-        v.push_back(0);
-        
-        // vis[i] : 점수 i가 나온적 있으면 true
-        bool vis[10001];
-        fill(vis, vis + 10001, false);
-        vis[0] = true;
-        
+        bool isUsed[10001];
+        fill(isUsed, isUsed + 10001, false);
+
+        vector<int> possible;
+        possible.push_back(0);
+        isUsed[0] = true;
+
         for(int i = 0; i < n; i++)
         {
-            int m = v.size() - 1;
-            for(int j = m; j >= 0; j--)
+            int m = (int)possible.size();
+            for(int j = m - 1; j >= 0; j--)
             {
-                int cur = v[j];
-                int nxt = v[j] + scores[i];
-                
-                if(!vis[nxt])
+                int check = score[i] + possible[j];
+                if(!isUsed[check])
                 {
-                    vis[nxt] = true;
-                    v.push_back(nxt);
+                    isUsed[check] = true;
+                    possible.push_back(check);
                 }
             }
         }
-        
-        cout << "#" << t << " " << v.size() << "\n";
+
+        cout << "#" << t << " " << (int)possible.size() << "\n";
     }
     return 0;
 }
