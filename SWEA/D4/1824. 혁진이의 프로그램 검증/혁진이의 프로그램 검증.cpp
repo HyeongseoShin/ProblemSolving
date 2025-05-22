956da34673d6bc9767c8f8e22eb483f9b49c1ead
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-22 16:02:34 +0900

    [D4] Title: 혁진이의 프로그램 검증, Time: 13 ms, Memory: 6,024 KB -BaekjoonHub

--- SWEA/D4/1824. 혁진이의 프로그램 검증/혁진이의 프로그램 검증.cpp
+++ SWEA/D4/1824. 혁진이의 프로그램 검증/혁진이의 프로그램 검증.cpp
@@ -4,57 +4,69 @@ using namespace std;
 
 int r, c;
 
-// 우, 좌, 상, 하
-int dx[4] = {0, 0, -1, 1};
-int dy[4] = {1, -1, 0, 0};
-
 char board[20][20];
 
-bool isPossible = false;
-
-// 방문 확인 배열
-// [x좌표][y좌표][진행방향][메모리값]
+// x좌표, y좌표, 들어온 방향, 메모리
 bool vis[20][20][4][16];
+
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
 
 bool BFS()
 {
-    // <x, y> <dir, memory>
-    queue<pair<pair<int,int>, pair<int,int>>> q;
+    queue<pair<pair<int, int>, pair<int, int>>> q;
+    q.push({{0, 0}, {1, 0}});
 
-    
-    vis[0][0][0][0] = true;
-
-    q.push({{0, 0}, {0, 0}});
+    vis[0][0][1][0] = true;
 
     while(!q.empty())
     {
         auto [x, y] = q.front().first;
-        auto [dir, memory] = q.front().second;
-
-        if(board[x][y] == '@') return true;
+        auto [dir, mem] = q.front().second;
 
         q.pop();
 
-        char op = board[x][y];
+        if(board[x][y] == '@') return true;
 
-        if(op == '<') dir = 1;
-        else if(op == '>') dir = 0;
-        else if(op == '^') dir = 2;
-        else if(op == 'v') dir = 3;
+        else if(board[x][y] == '<') dir = 0;
+        else if(board[x][y] == '>') dir = 1;
+        else if(board[x][y] == '^') dir = 2;
+        else if(board[x][y] == 'v') dir = 3;
 
-        else if(op == '_') dir = (memory == 0) ? 0 : 1;
-        else if(op == '|') dir = (memory == 0) ? 3 : 2;
+        else if(board[x][y] == '_') dir = (mem == 0) ? 1 : 0;
+        else if(board[x][y] == '|') dir = (mem == 0) ? 3 : 2;
+
+        else if(board[x][y] >= '0' && board[x][y] <= '9') mem = board[x][y] - '0';
         
-        else if(op >= '0' && op <= '9') memory = op - '0';
-
-        else if(op == '+') memory = (memory + 1) % 16;
-        else if(op == '-')
+        else if(board[x][y] == '+')
         {
-            memory--;
-            if(memory < 0) memory = 15;
+            mem++;
+            if(mem >= 16) mem = 0;
+        }
+        else if(board[x][y] == '-')
+        {
+            mem--;
+            if(mem < 0) mem = 15;
         }
 
-        if(op == '?')
+        if(board[x][y] != '?')
+        {
+            int nX = x + dx[dir];
+            int nY = y + dy[dir];
+
+            if(nX < 0) nX = r - 1;
+            else if(nX >= r) nX = 0;
+            
+            if(nY < 0) nY = c - 1;
+            else if(nY >= c) nY = 0;
+
+            if(vis[nX][nY][dir][mem]) continue;
+            
+            vis[nX][nY][dir][mem] = true;
+            q.push({{nX, nY}, {dir, mem}});
+        }
+
+        else
         {
             for(int i = 0; i < 4; i++)
             {
@@ -63,36 +75,19 @@ bool BFS()
 
                 if(nX < 0) nX = r - 1;
                 else if(nX >= r) nX = 0;
-
+                
                 if(nY < 0) nY = c - 1;
-                else if (nY >= c) nY = 0;
+                else if(nY >= c) nY = 0;
 
-                if(vis[nX][nY][i][memory]) continue;
-
-                vis[nX][nY][i][memory] = true;
-                q.push({{nX, nY}, {i, memory}});
+                if(vis[nX][nY][i][mem]) continue;
+                
+                vis[nX][nY][i][mem] = true;
+                q.push({{nX, nY}, {i, mem}});
             }
-        }
-        else
-        {
-            x += dx[dir];
-            y += dy[dir];
-
-            if(x < 0) x = r - 1;
-            else if(x >= r) x = 0;
-
-            if(y < 0) y = c - 1;
-            else if (y >= c) y = 0;
-
-            if(vis[x][y][dir][memory]) continue;
-
-            vis[x][y][dir][memory] = true;
-            q.push({{x, y}, {dir, memory}});
         }
     }
 
     return false;
-
 }
 
 int main()
@@ -106,7 +101,7 @@ int main()
     for(int t = 1; t <= testCase; t++)
     {
         cin >> r >> c;
-        
+
         for(int i = 0; i < r; i++)
         {
             for(int j = 0; j < c; j++)
@@ -121,5 +116,6 @@ int main()
         if(BFS()) cout << "YES\n";
         else cout << "NO\n";
     }
+
     return 0;
 }
