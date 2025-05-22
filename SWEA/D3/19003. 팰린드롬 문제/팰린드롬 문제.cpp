81a1e9ab41326a50881ac47e5495e4e6a97bac73
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-22 10:48:11 +0900

    [D3] Title: 팰린드롬 문제, Time: 11 ms, Memory: 5,996 KB -BaekjoonHub

--- SWEA/D3/19003. 팰린드롬 문제/팰린드롬 문제.cpp
+++ SWEA/D3/19003. 팰린드롬 문제/팰린드롬 문제.cpp
@@ -2,54 +2,76 @@
 
 using namespace std;
 
+vector<string> v;
+
+int n, m;
+
+int ans = 0;
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
-    int test;
-    cin >> test;
 
-    for(int t = 1; t <= test; t++)
+    int testCase;
+    cin >> testCase;
+
+    for(int t = 1; t <= testCase; t++)
     {
-        int n, m;
         cin >> n >> m;
-        
-        int cnt = 0;
-        
-        vector<string> v;
-        
+
+        v.clear();
         for(int i = 0; i < n; i++)
         {
             string s;
             cin >> s;
-            
-            if(s != string(s.rbegin(), s.rend()))	v.push_back(s);
-            else cnt = 1;
+
+            v.push_back(s);
         }
-        
-        if(!v.empty())
+
+        // 가장 긴 팰린드롬 찾기
+        int ans = 0;
+        for(int i = 0; i < n; i++)
         {
-            for(int i = 0; i < (int)v.size() - 1; i++)
+            int l = 0;
+            int r = (int)v[i].length() - 1;
+
+            while(l <= r && v[i][l] == v[i][r])
             {
-                // if(v[i] == "") continue;
-                for(int j = i + 1; j < (int)v.size(); j++)
+                l++;
+                r--;
+            }
+
+            // cout << "l: " << l << " r:" << r << "\n";
+            if(l > r) ans = max(ans, (int)v[i].length());
+        }
+
+        bool isUsed[101];
+        fill(isUsed, isUsed + 101, false);
+
+        // 현재 문자열과 짝이 되는 문자열이 있는지 확인
+        for(int i = 0; i < n - 1; i++)
+        {
+            if(isUsed[i]) continue;
+
+            string tmp = v[i];
+            reverse(tmp.begin(), tmp.end());
+
+            for(int j = i + 1; j < n; j++)
+            {
+                if(tmp == v[j])
                 {
-                    // if(v[j] == "") continue;
-                    if(v[i] == string(v[j].rbegin(), v[j].rend()))
-                    {
-                        // v[i] = "";
-                        // v[j] = "";
-                        cnt += 2;
-                        break;
-                    }
+                    isUsed[i] = true;
+                    isUsed[j] = true;
+
+                    ans += ((int)v[i].length() * 2);
                 }
             }
         }
         
-        
-                   
-         cout << "#" << t << " " << cnt * m << "\n";
+        cout << "#" << t << " " << ans << "\n";
     }
+
+    
     return 0;
 }
