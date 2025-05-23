5499fb8f82bff68fab529d7856304e4e20bc695e
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-05-23 22:23:52 +0900

    [D3] Title: 프리셀 통계, Time: 5 ms, Memory: 5,988 KB -BaekjoonHub

--- SWEA/D3/12051. 프리셀 통계/프리셀 통계.cpp
+++ SWEA/D3/12051. 프리셀 통계/프리셀 통계.cpp
@@ -8,43 +8,35 @@ int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     int testCase;
     cin >> testCase;
-    
+
     for(int t = 1; t <= testCase; t++)
     {
+        ll n, pD, pG;
+
+        cin >> n >> pD >> pG;
+
         bool isPossible = false;
-        
-        ll n, d, g;
-        cin >> n  >> d >> g;
-        
-        cout << "#" << t << " ";
-        
-        if((d != 0 && g == 0) || (d != 100 && g == 100))
-        {
-            cout << "Broken\n";
-			continue;
-        }
-        
+        if(pG == 100 && pD != 100) isPossible = false;
+        else if(pG == 0 && pD != 0) isPossible = false;
 
         else
         {
             for(ll i = 1; i <= n; i++)
             {
-				if(i * d % 100 == 0)
+                if(i * pD % 100 == 0)
                 {
                     isPossible = true;
                     break;
                 }
             }
-
         }
-
+        
+        cout << "#" << t << " ";
         if(isPossible) cout << "Possible\n";
         else cout << "Broken\n";
-        
-        
     }
     return 0;
 }
