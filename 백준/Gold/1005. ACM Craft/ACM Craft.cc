f9344e2d320f72503d8d985800953834d73b72a1
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-06-27 14:43:17 +0900

    [Gold III] Title: ACM Craft, Time: 136 ms, Memory: 2852 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.cc
+++ 백준/Gold/1005. ACM Craft/ACM Craft.cc
@@ -4,34 +4,11 @@ using namespace std;
 
 int t, n, k, w;
 
-int d[1001];
+int d[1001]; // 각 건물 건설 시간
+int inDegree[1001]; // 진입 차수 (위상 정렬)
+vector<int> adj[1001]; // 건설 순서 그래프
+int dp[1001]; // dp[i] : i번째 건물 짓기까지 걸리는 최소 시간
 
-int dist[1001];
-
-vector<int> adj[1001];
-
-void BFS(int x)
-{
-    dist[x] = d[x];
-
-    queue<int> q;
-    q.push(x);
-
-    while(!q.empty())
-    {
-        int cur = q.front();
-        q.pop();
-
-        for(auto nxt : adj[cur])
-        {
-            if(dist[nxt] < dist[cur] + d[nxt])
-            {
-                dist[nxt] = dist[cur] + d[nxt];
-                q.push(nxt);
-            }
-        }
-    }
-}
 int main()
 {
     ios::sync_with_stdio(0);
@@ -46,11 +23,9 @@ int main()
         for(int i = 1; i <= n; i++)
         {
             cin >> d[i];
-        }
-
-        for(int i = 1; i <= 1000; i++)
-        {
             adj[i].clear();
+            inDegree[i] = 0;
+            dp[i] = 0;
         }
 
         while(k--)
@@ -59,22 +34,43 @@ int main()
             cin >> u >> v;
 
             adj[u].push_back(v);
+            inDegree[v]++; // 위상 정렬 위해 진입 차수 ++ (먼저 지어져야 할 건물 수)
         }
 
         cin >> w;
 
+        queue<int> q;
 
-        fill(dist, dist + 1001, 0);
-
+        // 먼저 지을 수 있는 건물 큐에 삽입
         for(int i = 1; i <= n; i++)
         {
-            if(dist[i] == 0)
+            if(inDegree[i] == 0)
             {
-                BFS(i);
-            }
+                q.push(i);
+                dp[i] = d[i];
+            }   
         }
 
-        cout << dist[w] << "\n";
+        // 위상 정렬
+        while(!q.empty())
+        {
+            int cur = q.front();
+            q.pop();
+
+            for(int nxt : adj[cur])
+            {
+                dp[nxt] = max(dp[nxt], dp[cur] + d[nxt]);
+
+                inDegree[nxt]--;
+                if(inDegree[nxt] == 0)
+                {
+                    q.push(nxt);
+                }
+            }
+        }
+        
+
+        cout << dp[w] << "\n";
     }
     
     return 0;
