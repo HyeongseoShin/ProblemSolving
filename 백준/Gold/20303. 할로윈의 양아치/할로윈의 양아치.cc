fa5f217234439b04c8068228c11657bdd252f78b
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-07-08 18:12:21 +0900

    [Gold II] Title: 할로윈의 양아치, Time: 260 ms, Memory: 356544 KB -BaekjoonHub

---
+++ 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.cc
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m, k;
+
+int candy[30001];
+
+vector<int> adj[30001];
+
+vector<pair<int,int>> amount; // <사탕 수, 뺏은 사람 수>
+
+bool vis[30001];
+
+// dp[i][j] : 현재 i번째까지 봤고, j 사람의 사탕을 뺏었을 때 최대 사탕 수
+int dp[30001][3001];
+
+// BFS 순회하며 amount 채우기
+void BFS(int v)
+{
+    vis[v] = true;
+    
+    queue<int> q;
+    q.push(v);
+
+    int sum = candy[v]; // 뺏은 사탕 수
+    int cnt = 1; // 사탕 뺏긴 사람 수
+
+    while(!q.empty())
+    {
+        int cur = q.front();
+        q.pop();
+
+        for(int nxt : adj[cur])
+        {
+            if(vis[nxt]) continue;
+
+            q.push(nxt);
+            vis[nxt] = true;
+            sum += candy[nxt];
+            cnt++;
+        }
+    }
+
+    amount.push_back({sum, cnt});
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m >> k;
+
+    for(int i = 1; i <= n; i++) cin >> candy[i];
+    
+    for(int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+
+    // 1-indexed 처리 위해 하나 미리 넣어줌
+    // amount.push_back({0, 0});
+
+    // 뺏을 수 있는 사탕, 사람 수 구하기
+    for(int i = 1; i <= n; i++)
+    {
+        if(!vis[i]) BFS(i);
+    }
+
+    
+
+    int amountLen = (int)amount.size();
+    auto [c, p] = amount[0];
+    dp[0][p] = c;
+
+    // for(int i = 0; i < amountLen; i++)
+    // {
+    //     auto [c, p] = amount[i];
+
+    //     cout << "c: " << c << " p: " << p << "\n";
+    // }
+
+    for(int i = 1; i < amountLen; i++)
+    {
+        auto [c, p] = amount[i];
+
+        for(int j = 1; j < k; j++)
+        {
+            if(p <= j)
+            {
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j - p] + c);
+            }
+
+            else dp[i][j] = dp[i-1][j];
+
+            // cout << "dp[" << i << "][" << j << "] : " << dp[i][j] << "\n";
+        }
+    }
+
+    int ans = 0;
+
+    // cout << "amountLen: " << amountLen << "\n";
+    for(int i = 0; i < k; i++)
+    {
+        if(ans < dp[amountLen - 1][i])
+        {
+            ans = max(ans, dp[amountLen - 1][i]);
+            // cout << "i: " << i << "\n";
+        }
+        
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
