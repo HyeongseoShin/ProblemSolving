8cb119a9d8a31847ba05e3cb3a5c734550e545b4
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-07-08 18:24:04 +0900

    [Gold II] Title: 할로윈의 양아치, Time: 84 ms, Memory: 4864 KB -BaekjoonHub

--- 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.cc
+++ 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.cc
@@ -1,63 +1,84 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int n, m, k;
+
 int candy[30001];
+
 vector<int> adj[30001];
+
+vector<pair<int,int>> amount; // <사탕 수, 뺏은 사람 수>
+
 bool vis[30001];
 
-vector<pair<int, int>> amount;  // {사탕 합, 인원 수}
+// dp[i] : i명의 사탕을 뺏었을 때 최대 사탕 수
+int dp[3001];
 
-void bfs(int start) {
+// BFS 순회하며 amount 채우기
+void BFS(int v)
+{
+    vis[v] = true;
+    
     queue<int> q;
-    q.push(start);
-    vis[start] = true;
+    q.push(v);
 
-    int cnt = 1;
-    int totalCandy = candy[start];
+    int sum = candy[v]; // 뺏은 사탕 수
+    int cnt = 1; // 사탕 뺏긴 사람 수
 
-    while (!q.empty()) {
-        int cur = q.front(); q.pop();
-        for (int nxt : adj[cur]) {
-            if (!vis[nxt]) {
-                vis[nxt] = true;
-                totalCandy += candy[nxt];
-                cnt++;
-                q.push(nxt);
-            }
+    while(!q.empty())
+    {
+        int cur = q.front();
+        q.pop();
+
+        for(int nxt : adj[cur])
+        {
+            if(vis[nxt]) continue;
+
+            q.push(nxt);
+            vis[nxt] = true;
+            sum += candy[nxt];
+            cnt++;
         }
     }
 
-    amount.push_back({totalCandy, cnt});
+    amount.push_back({sum, cnt});
 }
 
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     cin >> n >> m >> k;
 
-    for (int i = 1; i <= n; i++) cin >> candy[i];
+    for(int i = 1; i <= n; i++) cin >> candy[i];
+    
+    for(int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
 
-    for (int i = 0; i < m; i++) {
-        int u, v; cin >> u >> v;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
 
-    // 그룹 단위로 묶기
-    for (int i = 1; i <= n; i++) {
-        if (!vis[i]) bfs(i);
+    // 뺏을 수 있는 사탕, 사람 수 구하기
+    for(int i = 1; i <= n; i++)
+    {
+        if(!vis[i]) BFS(i);
     }
 
-    // 1차원 DP 배열로 냅색 문제 해결
-    vector<int> dp(k, 0);  // dp[j] = j명의 아이를 뺏었을 때 최대 사탕 수
-
-    for (auto [c, p] : amount) {
-        for (int j = k - 1; j >= p; j--) {
-            dp[j] = max(dp[j], dp[j - p] + c);
+    for(int i = 0; i < (int)amount.size(); i++)
+    {
+        auto [c, p] = amount[i];
+        for(int j = k - 1; j >= p; j--)
+        {
+            dp[j] = max(dp[j], dp[j-p] + c);
         }
     }
 
-    cout << *max_element(dp.begin(), dp.end()) << "\n";
+    cout << *max_element(dp, dp + k) << "\n";
+
     return 0;
 }
