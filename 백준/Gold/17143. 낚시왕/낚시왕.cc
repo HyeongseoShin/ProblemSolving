6c31ffb8de9088f8eaef8994766cecc81813fcd8
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-07-13 16:26:00 +0900

    [Gold I] Title: 낚시왕, Time: 152 ms, Memory: 2140 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.cc
+++ 백준/Gold/17143. 낚시왕/낚시왕.cc
@@ -1,134 +1,89 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int r, c, m;
+struct Shark {
+    int s, d, z; // 속력, 방향, 크기
+};
 
-int dx[5] = {0, -1, 1, 0, 0};
-int dy[5] = {0, 0, 0, 1, -1};
+int R, C, M;
+int dx[] = {0, -1, 1, 0, 0}; // 위, 아래, 오른쪽, 왼쪽
+int dy[] = {0, 0, 0, 1, -1};
 
-// <크기 , 속력, 방향>
-tuple<int, int, int> beforeShark[101][101]; // 이동 전 상어
-priority_queue<tuple<int, int, int>> afterShark[101][101]; // 이동 후 상어
+Shark board[101][101];
 
-int ans = 0;
+void moveSharks() {
+    Shark newBoard[101][101] = {};
 
-void MoveShark()
-{
-    for(int i = 1; i <= r; i++)
-    {
-        for(int j = 1; j <= c; j++)
-        {
-            // (크기, 속력, 방향)
-            auto [z, s, d] = beforeShark[i][j];
+    for (int i = 1; i <= R; i++) {
+        for (int j = 1; j <= C; j++) {
+            Shark &shark = board[i][j];
+            if (shark.z == 0) continue;
 
-            // 방향 0이면 상어 없음
-            if(d == 0) continue;
+            int x = i, y = j;
+            int s = shark.s, d = shark.d, z = shark.z;
 
-            int nX = i, nY = j;
-            int tmpS = s;
+            // 이동 최적화
+            if (d == 1 || d == 2) s %= (2 * (R - 1));
+            else s %= (2 * (C - 1));
 
-            if(d == 1 || d == 2) tmpS %= 2 * (r - 1);
-            else tmpS %= 2 * (c - 1);
+            while (s--) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
 
-            while(tmpS--)
-            {
-                int nx = nX + dx[d];
-                int ny = nY + dy[d];
+                if (nx < 1 || nx > R || ny < 1 || ny > C) {
+                    if (d == 1) d = 2;
+                    else if (d == 2) d = 1;
+                    else if (d == 3) d = 4;
+                    else if (d == 4) d = 3;
 
-                if(nx < 1 || nx > r || ny < 1 || ny > c)
-                {
-                    if(d == 1 || d == 3) d++;
-                    else d--;
-                    
-                    nx = nX + dx[d];
-                    ny = nY + dy[d];
+                    nx = x + dx[d];
+                    ny = y + dy[d];
                 }
 
-                nX = nx;
-                nY = ny;
+                x = nx;
+                y = ny;
             }
 
-            // cout << "nX: " << nX << " nY: " << nY << "\n";
-
-            // 원래 지점 상어 없어짐
-            beforeShark[i][j] = {0, 0, 0};
-
-            // 새로운 곳에 상어 추가
-            afterShark[nX][nY].push({z, s, d});
+            // 겹치면 크기 비교
+            if (newBoard[x][y].z < z)
+                newBoard[x][y] = {shark.s, d, z};
         }
     }
 
-    // afterShark -> beforeShark로 복사
-    for(int i = 1; i <= r; i++)
-    {
-        for(int j = 1; j <= c; j++)
-        {
-            if(afterShark[i][j].empty()) continue;
-
-            beforeShark[i][j] = afterShark[i][j].top();
-
-            while(!afterShark[i][j].empty()) afterShark[i][j].pop();
-        }
-    }
-
-    // cout << "\n=======Before Shark=======\n";
-    // // BeforeShark
-    // for(int i = 1; i <= r; i++)
-    // {
-    //     for(int j = 1; j <= c; j++)
-    //     {
-    //         auto [z, s, d] = beforeShark[i][j];
-    //         cout << z << " ";
-    //     }
-    //     cout << "\n";
-    // }
+    // board 갱신
+    for (int i = 1; i <= R; i++)
+        for (int j = 1; j <= C; j++)
+            board[i][j] = newBoard[i][j];
 }
-int main()
-{
-    ios::sync_with_stdio(0);
+
+int main() {
+    ios::sync_with_stdio(false);
     cin.tie(0);
 
-    cin >> r >> c >> m;
+    cin >> R >> C >> M;
 
-    // 상어 위치, 속력, 방향, 크기 저장
-    for(int i = 0; i < m; i++)
-    {
-        int rPos, cPos, s, d, z;
-
-        cin >> rPos >> cPos >> s >> d >> z;
-
-        // <크기, 속력, 방향>
-        beforeShark[rPos][cPos] = {z, s, d};
+    for (int i = 0; i < M; i++) {
+        int r, c, s, d, z;
+        cin >> r >> c >> s >> d >> z;
+        board[r][c] = {s, d, z};
     }
 
-    
-    // 1. 낚시와 오른쪽으로 한 칸 이동
-    for(int i = 1; i <= c; i++)
-    {
-        // 2. 낚시왕이 현재 열에 있는 상어 중 땅과 제일 가까운 상어 잡음
-        for(int j = 1; j <= r; j++)
-        {
-            // <크기, 속력, 방향>
-            auto [z, s, d] = beforeShark[j][i];
+    int answer = 0;
 
-            // 방향이 0이면 상어 없음
-            if(d == 0) continue;
-
-            // cout << "z: " << z << " s: " << s << " d: " << d << "\n";
-
-            // 상어 잡고 상어 사라짐
-            ans += z;
-            beforeShark[j][i] = {0, 0, 0};
-
-            break;
+    for (int col = 1; col <= C; col++) {
+        // 1. 낚시왕이 상어 잡기
+        for (int row = 1; row <= R; row++) {
+            if (board[row][col].z > 0) {
+                answer += board[row][col].z;
+                board[row][col] = {0, 0, 0}; // 잡힘
+                break;
+            }
         }
 
-        // 3. 상어 이동
-        MoveShark();
+        // 2. 상어 이동
+        moveSharks();
     }
-    
-    cout << ans << "\n";
 
+    cout << answer << '\n';
     return 0;
 }
