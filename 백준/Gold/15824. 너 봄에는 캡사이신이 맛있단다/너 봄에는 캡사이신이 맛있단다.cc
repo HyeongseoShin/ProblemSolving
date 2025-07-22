c97da2c2aea686558eb54d17eff1f2ddc6446690
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-07-22 09:26:24 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 0 ms, Memory: 2184 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define MOD 1000000007
+
+int n;
+
+vector<int> v;
+
+ll ans = 0;
+
+ll pow2[3001];
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+
+        v.push_back(x);
+    }
+
+    sort(v.begin(), v.end());
+
+    pow2[0] = 1;
+
+    for(int i = 1; i < n; i++)
+    {
+        pow2[i] = (pow2[i-1] * 2) % MOD;
+    }
+
+    // A[i]가 가장 큰 수 일때 => iC0 + ~ + iCi => 2^i
+    // A[i]가 가장 작은 수 일때 => n-(i+1)C0 + ~ + n-(i+1)Cn-(i+1) => 2^(n - (i+1))
+
+    for(int i = 0; i < n; i++)
+    {
+        ll whenMax = (v[i] * pow2[i]) % MOD;
+        ll whenMin = (v[i] * pow2[n - (i+1)]) % MOD;
+
+        ans = (ans + whenMax - whenMin + MOD) % MOD;
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
