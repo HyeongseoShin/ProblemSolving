41b44e7be9457250a61a151971115ecc949b2feb
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-08-08 16:39:15 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 32 ms, Memory: 4008 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.cc
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.cc
@@ -5,37 +5,33 @@ using namespace std;
 int n;
 
 int board[501][501];
-int dist[501][501];
+int dp[501][501];
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
-priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
-
 int ans = 1;
 
-void BFS()
+int DFS(int x, int y)
 {
-    while(!pq.empty())
+    if(dp[x][y] != 0) return dp[x][y];
+    dp[x][y] = 1;
+
+    for(int i = 0; i < 4; i++)
     {
-        auto [curVal, curX, curY] = pq.top();
-        pq.pop();
+        int nX = x + dx[i];
+        int nY = y + dy[i];
 
-        for(int i = 0; i < 4; i++)
-        {
-            int nX = curX + dx[i];
-            int nY = curY + dy[i];
+        if(nX < 0 || nX >= n || nY < 0 || nY >= n) continue;
+        if(board[nX][nY] <= board[x][y]) continue;
 
-            if(nX < 0 || nX >= n || nY < 0 || nY >= n) continue;
-            if(board[nX][nY] <= board[curX][curY]) continue;
-            if(dist[nX][nY] <= dist[curX][curY] + 1)
-            {
-                dist[nX][nY] = dist[curX][curY] + 1;
-                ans = max(ans, dist[nX][nY]);
-            }
-        }
+        dp[x][y] = max(dp[x][y], DFS(nX, nY) + 1);
+        
     }
+
+    return dp[x][y];
 }
+
 int main()
 {
     ios::sync_with_stdio(0);
@@ -48,25 +44,18 @@ int main()
         for(int j = 0; j < n; j++)
         {
             cin >> board[i][j];
-            pq.push({board[i][j], i, j});
-            dist[i][j] = 1;
         }
     }
 
-    BFS();
-
-    // cout << "\n===============\n";
-    // for(int i = 0; i < n; i++)
-    // {
-    //     for(int j = 0; j < n; j++)
-    //     {
-    //         cout << dist[i][j] << " ";
-    //     }
-    //     cout << "\n";
-    // }
+    for(int i = 0; i < n; i++) 
+    {
+        for(int j = 0; j < n; j++)
+        {
+            ans = max(ans, DFS(i, j));
+        }
+    }
 
     cout << ans << "\n";
-
 
     return 0;
 }
