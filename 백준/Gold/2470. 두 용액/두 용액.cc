03c5c6bcac53ac6be9db038d4d20f4e16eb645a0
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-08-18 09:42:53 +0900

    [Gold V] Title: 두 용액, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+vector<int> v;
+
+pair<int, int> ans;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+
+        v.push_back(x);
+    }
+
+    sort(v.begin(), v.end());
+
+    int st = 0;
+    int en = n - 1;
+
+    ans = {1000000000, 1000000000};
+
+    while(st < en)
+    {
+        int mid = (st + en) / 2;
+
+        // cout << "st: " << st << " mid: " << mid <<  " en : " << en << "\n";
+
+        if(abs(v[st] + v[en]) < abs(ans.first + ans.second))
+        {
+            ans = {v[st], v[en]};
+        }
+
+        if(v[st] + v[en] >= 0) en--;
+        else st++;
+    }
+
+    cout << ans.first << " " << ans.second << "\n";
+
+    return 0;
+}
