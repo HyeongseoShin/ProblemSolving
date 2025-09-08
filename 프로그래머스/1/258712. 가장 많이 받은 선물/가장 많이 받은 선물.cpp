49dc3d8f585eff28e046239fdecdd031d241ae54
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-09-08 17:22:33 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 6.71 ms, Memory: 4.27 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -6,8 +6,6 @@ using namespace std;
 // <이름, 인덱스>
 unordered_map<string, int> nameToIdx;
 
-unordered_map<int, string> idxToName;
-
 // 선물 주고 받은 횟수 저장
 int cnt[51][51];
 
@@ -24,11 +22,7 @@ int solution(vector<string> friends, vector<string> gifts) {
     int n = (int)friends.size();
     
     // <친구 이름, 인덱스> 저장
-    for(int i = 0; i < n; i++)
-    {
-        nameToIdx[friends[i]] = i;
-        idxToName[i] = friends[i];
-    }
+    for(int i = 0; i < n; i++) nameToIdx[friends[i]] = i;
     
     int m = (int) gifts.size();
     
@@ -57,27 +51,10 @@ int solution(vector<string> friends, vector<string> gifts) {
         score[toIdx]--;
     }
     
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            cout << cnt[i][j] << " ";
-        }
-        cout << "\n";
-    }
-    
-    cout << "\n";
-    for(int i = 0; i < n; i++)
-    {
-        cout << score[i] << " ";
-    }
-    cout << "\n";
-    
     // 선물 준 사람
     for(int i = 0; i < n - 1; i++)
     {
         int from = i;
-        string fromName = idxToName[from];
         
         // 선물 받은 사람
         for(int j = i+1; j < n; j++)
@@ -85,36 +62,18 @@ int solution(vector<string> friends, vector<string> gifts) {
             if(i == j) continue;
             
             int to = j;
-            string toName = idxToName[to];
             
             // 선물 주고 받은 기록 있다면 더 많이 준 사람이 선물 받기
-            if(cnt[from][to] > cnt[to][from]) 
-            {
-                ans[from]++;
-                cout << fromName << "이 " << toName << "에게 선물 받음" << "\n";        
-            }
-            else if(cnt[from][to] < cnt[to][from])
-            {
-                ans[to]++;
-                cout << toName << "이 " << fromName << "에게 선물 받음" << "\n";
-            }
+            if(cnt[from][to] > cnt[to][from]) ans[from]++;
+            else if(cnt[from][to] < cnt[to][from]) ans[to]++;
             
             // 선물 기록 없거나 주고 받은 횟수 같다면 선물지수 더 큰 사람이 받기
-            else if(score[from] > score[to])
-            {
-                ans[from]++;
-                cout << fromName << "이 " << toName << "에게 선물 받음" << "\n";
-            }
-            else if(score[from] < score[to])
-            {
-                ans[to]++;
-                cout << toName << "이 " << fromName << "에게 선물 받음" << "\n";
-            }
+            else if(score[from] > score[to]) ans[from]++;
+            else if(score[from] < score[to]) ans[to]++;
         }
     }
     
     sort(ans, ans + n);
-    
     
     return ans[n-1];
 }
