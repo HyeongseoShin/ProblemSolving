e93275db29ae1ea1e48db47d66a7073557e82bdb
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-10-16 10:36:22 +0900

    [Gold III] Title: 움직이는 미로 탈출, Time: 36 ms, Memory: 23260 KB -BaekjoonHub

--- 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.cc
+++ 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.cc
@@ -7,9 +7,6 @@ string board[8];
 int dx[9] = {0, -1, -1, 0, 1, 1, 1, 0, -1};
 int dy[9] = {0, 0, 1, 1, 1, 0, -1, -1, -1};
 
-// 해당 위치가 벽이 되는 시간
-set<int> wallTime[8][8];
-
 void printBoard()
 {
     cout << "\n";
@@ -24,43 +21,8 @@ void printBoard()
     cout << "\n";
 }
 
-int BFS()
-{
-    bool vis[8][8];
-    memset(vis, false, sizeof(vis));
-
-    // <x, y, 현재 시간>
-    queue<tuple<int, int, int>> q;
-    q.push({7, 0, 0});
-    vis[7][0] = true;
-
-    while(!q.empty())
-    {
-        auto [curX, curY, curT] = q.front();
-        q.pop();
-
-        for(int i = 0; i < 9; i++)
-        {
-            int nX = curX + dx[i];
-            int nY = curY + dy[i];
-            int nT = curT + 1;
-
-            if(nX < 0 || nX >= 8 || nY < 0 || nY >= 8) continue; // 범위 넘으면 패스
-            if(wallTime[nX][nY].count(nT) > 0 || wallTime[nX][nY].count(curT) > 0) continue; // 이동할 위치에 도착하는 시간에 벽이라면 패스
-
-            // 도착점 도착 시 끝
-            if(nX == 0 && nY == 7) return 1;
-
-            // if(curT == 0) cout << "nX: " << nX << " nY: " << nY << " nT: " << nT << "\n";
-            q.push({nX, nY, curT + 1});
-        }
-    }
-
-    return 0;
-}
-
-// 좌표가 벽이 되는 시간을 저장
-void moveWall(int t)
+// 벽 이동
+void moveWall()
 {
     for(int i = 7; i >= 0; i--)
     {
@@ -74,11 +36,48 @@ void moveWall(int t)
                 {
                     board[i][j] = '.';
                     board[i+1][j] = '#';
-                    wallTime[i+1][j].insert(t);
                 }
             }
         }
     }
+}
+
+int BFS()
+{
+    // <x, y, 현재 시간>
+    queue<tuple<int, int>> q;
+    q.push({7, 0});
+
+    while(!q.empty())
+    {
+        int sz = (int)q.size();
+
+        while(sz--)
+        {
+            auto [curX, curY] = q.front();
+            q.pop();
+
+            if(board[curX][curY] == '#') continue;
+
+            for(int i = 0; i < 9; i++)
+            {
+                int nX = curX + dx[i];
+                int nY = curY + dy[i];
+
+                if(nX < 0 || nX >= 8 || nY < 0 || nY >= 8) continue; // 범위 넘으면 패스
+                if(board[nX][nY] == '#') continue;
+
+                // 도착점 도착 시 끝
+                if(nX == 0 && nY == 7) return 1;
+
+                q.push({nX, nY});
+            }
+        }
+
+        moveWall();
+    }
+
+    return 0;
 }
 
 int main()
@@ -86,16 +85,7 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    for(int i = 0; i < 8; i++)
-    {
-        cin >> board[i];
-        for(int j = 0; j < 8; j++)
-        {
-            if(board[i][j] == '#') wallTime[i][j].insert(0);
-        }
-    }
-
-    for(int i = 1; i <= 8; i++) moveWall(i);
+    for(int i = 0; i < 8; i++) cin >> board[i];
 
     cout << BFS() << "\n";
 
