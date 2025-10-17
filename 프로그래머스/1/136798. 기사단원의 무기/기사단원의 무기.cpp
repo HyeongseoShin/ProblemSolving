6246167409b0eaab23bafb99db011944b6ff4272
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-10-17 22:13:05 +0900

    [level 1] Title: 기사단원의 무기, Time: 65.40 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -6,20 +6,15 @@ int ans = 0;
 int solution(int number, int limit, int power) {
     for(int i = 1; i <= number; i++)
     {
-        unordered_set<int> us;
-        for(int j = 1; j <= i; j++)
+        int cnt = 0;
+        for(int j = 1; j * j <= i; j++)
         {
-            if(i % j == 0)
-            {
-                if(us.count(j) > 0) break;
-                
-                us.insert(j);
-                us.insert(i / j);
-            }
+            if(j * j == i) cnt++;
+            else if(i % j == 0) cnt += 2;
         }
         
-        if((int)us.size() > limit) ans += power;
-        else ans += (int)us.size();
+        if(cnt > limit) ans += power;
+        else ans += cnt;
     }
     
     return ans;
