eaae2b7daa689442ac7df2b72c33d11a2236e904
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-10-17 11:32:15 +0900

    [Gold I] Title: 2048 (Easy), Time: 4 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
@@ -4,109 +4,212 @@ using namespace std;
 
 int n;
 
-int board[25][25];
-int board2[25][25];
+int board[21][21];
 
-// 2차원 배열 시계 방향 90도 회전하는 함수
-void Rotate() {
-    int tmp[25][25];
+bool isMerged[21][21]; // 이미 합쳐진 블록인가?
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            tmp[i][j] = board2[i][j];
+int ans = 0; // 정답
+
+void printBoard()
+{
+    cout << "\n===================\n";
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cout << board[i][j] << " ";
         }
+        cout << "\n";
     }
+    cout << "===================\n";
+}
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            board2[i][j] = tmp[n - 1 - j][i];
+void up()
+{
+    memset(isMerged, false, sizeof(isMerged));
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 1; j < n; j++)
+        {
+            // cout << "j: " << j << " i: " << i << "\n";
+            // 빈 칸이면 패스
+            if(board[j][i] == 0) continue;
+
+            int num = board[j][i];
+            board[j][i] = 0; // 현재 칸 비우기
+
+            int idx = j-1;
+
+            // 빈 칸이면 계속 이동
+            while(board[idx][i] == 0 && idx >= 1) idx--;
+
+            // 같은 숫자이고 아직 합쳐지지 않았으면
+            if(board[idx][i] == num && !isMerged[idx][i])
+            {
+                board[idx][i] *= 2;
+                ans = max(ans, board[idx][i]);
+                isMerged[idx][i] = true;
+            }
+
+            // 해당 칸이 빈 칸이면 바로 삽입
+            else if(board[idx][i] == 0) board[idx][i] = num;
+
+            // 다른 숫자이면 해당 칸 한 칸 아래에 위치
+            else board[idx+1][i] = num;
         }
     }
 }
 
-void Tilt(int dir) {
-    // 기본적으로 좌측으로 기울이는 것을 기준!
-    // 각 방향마다 기울이는 코드를 쓰는 것이 아니라 
-    // 배열을 회전시킨 것을 왼쪽으로 기울이는 것으로 함
+void down()
+{
+    memset(isMerged, false, sizeof(isMerged));
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = n-2; j >= 0; j--)
+        {
+            // cout << "j: " << j << " i: " << i << "\n";
+            // 빈 칸이면 패스
+            if(board[j][i] == 0) continue;
 
-    // 0 : 좌
-    // 1: 상
-    // 2: 우
-    // 3 : 하
-    while(dir--) {
-        Rotate();
-    }
+            int num = board[j][i];
+            board[j][i] = 0; // 현재 칸 비우기
 
-    for(int i = 0; i < n; i++) {
-        // board2를 기울인 결과 저장할 변수
-        int tilted[25] = {};
+            int idx = j+1;
+            while(board[idx][i] == 0 && idx <= n-2) idx++;
 
-        int idx = 0;
-
-        for(int j = 0 ; j < n; j++) {
-            if(board2[i][j] == 0) {
-                continue;
+            // 같은 숫자이고 아직 합쳐지지 않았으면
+            if(board[idx][i] == num && !isMerged[idx][i])
+            {
+                
+                board[idx][i] *= 2;
+                ans = max(ans, board[idx][i]);
+                isMerged[idx][i] = true;
             }
 
-            else if(tilted[idx] == 0) {
-                tilted[idx] = board2[i][j];
-            }
+            // 해당 칸이 빈 칸이면 바로 삽입
+            else if(board[idx][i] == 0) board[idx][i] = num;
 
-            else if(tilted[idx] == board2[i][j]) {
-                tilted[idx++] *= 2;
-            }
-
-            else {
-                tilted[++idx] = board2[i][j];
-            }
+            // 다른 숫자이면 해당 칸 한 칸 위에 위치
+            else board[idx-1][i] = num;
         }
-
-        for(int j = 0; j < n; j++) {
-            board2[i][j] = tilted[j];
-        }
-
     }
 }
 
+void left()
+{
+    memset(isMerged, false, sizeof(isMerged));
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 1; j < n; j++)
+        {
+            // cout << "j: " << j << " i: " << i << "\n";
+            // 빈 칸이면 패스
+            if(board[i][j] == 0) continue;
 
-int main() {
+            int num = board[i][j];
+            board[i][j] = 0; // 현재 칸 비우기
+
+            int idx = j-1;
+            while(board[i][idx] == 0 && idx >= 1) idx--;
+
+            // 같은 숫자이고 아직 합쳐지지 않았으면
+            if(board[i][idx] == num && !isMerged[i][idx])
+            {  
+                board[i][idx] *= 2;
+                ans = max(ans, board[i][idx]);
+                isMerged[i][idx] = true;  
+            }
+
+            // 해당 칸이 빈 칸이면 바로 삽입
+            else if(board[i][idx] == 0) board[i][idx] = num;
+
+            // 다른 숫자이면 해당 칸 한 칸 아래에 위치
+            else board[i][idx+1] = num;
+        }
+    }
+}
+
+void right()
+{
+    memset(isMerged, false, sizeof(isMerged));
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = n-2; j >= 0; j--)
+        {
+            // cout << "j: " << j << " i: " << i << "\n";
+            // 빈 칸이면 패스
+            if(board[i][j] == 0) continue;
+
+            int num = board[i][j];
+            board[i][j] = 0; // 현재 칸 비우기
+
+            int idx = j+1;
+            while(board[i][idx] == 0 && idx <= n-2) idx++;
+
+            // 같은 숫자이고 아직 합쳐지지 않았으면
+            if(board[i][idx] == num && !isMerged[i][idx])
+            {
+                board[i][idx] *= 2;
+                ans = max(ans, board[i][idx]);
+                isMerged[i][idx] = true;   
+            }
+
+            // 해당 칸이 빈 칸이면 바로 삽입
+            else if(board[i][idx] == 0) board[i][idx] = num;
+
+            // 다른 숫자이면 해당 칸 한 칸 위에 위치
+            else board[i][idx-1] = num;
+        }
+    }
+}
+
+void getAns(int cnt)
+{
+    // 5번 이동했으면 끝
+    if(cnt == 5) return;
+    
+    int tmp[21][21];
+    memcpy(tmp, board, sizeof(board));
+
+    // 상, 하, 좌, 우 이동
+    up();
+    // printBoard();
+    getAns(cnt+1);
+    memcpy(board, tmp, sizeof(tmp)); // 원상 복구
+
+    down();
+    // printBoard();
+    getAns(cnt+1);
+    memcpy(board, tmp, sizeof(tmp)); // 원상 복구
+
+    left();
+    // printBoard();
+    getAns(cnt+1);
+    memcpy(board, tmp, sizeof(tmp)); // 원상 복구
+
+    right();
+    // printBoard();
+    getAns(cnt+1);
+    memcpy(board, tmp, sizeof(tmp)); // 원상 복구   
+}
+
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> n;
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
             cin >> board[i][j];
+            ans = max(ans, board[i][j]);
         }
     }
 
-    int ans = 0;
-
-    // 4 ^ 5승 = 총 1024개의 경우의 수
-    for(int l = 0; l < (1 << (2 * 5)); l++) {
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                board2[i][j] = board[i][j];
-            }
-        }
-
-        int brute = l;
-
-        // 최대 5번 이동
-        for(int i = 0; i < 5; i++) {
-            int dir = brute % 4;
-            brute /= 4;
-
-            Tilt(dir);
-        }
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                ans = max(ans, board2[i][j]);
-            }
-        }
-    }
+    getAns(0);
 
     cout << ans << "\n";
 
