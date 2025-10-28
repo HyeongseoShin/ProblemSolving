d53d74be4cbd52a4e4091a8020faa38e3e610f08
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-10-28 11:31:27 +0900

    [Gold III] Title: 거울 설치, Time: 0 ms, Memory: 2204 KB -BaekjoonHub

--- 백준/Gold/2151. 거울 설치/거울 설치.cc
+++ 백준/Gold/2151. 거울 설치/거울 설치.cc
@@ -17,54 +17,54 @@ int stX = -1, stY = -1, enX = -1, enY = -1;
 
 int cnt[51][51][4]; // (x, y) 까지 dir 방향으로 오는 데에 설치한 최소 거울 개수
 
-void Dijkstra(int x, int y)
+// 0-1 BFS
+void BFS(int x, int y)
 {
-    // 거울 개수, x, y, 진행 방향
-    priority_queue<tuple<int, int, int, int>, vector<tuple<int, int, int, int>>, greater<>> pq;
+    // <x, y, 방향>
+    deque<tuple<int, int, int>> dq;
 
     for(int i = 0; i < 4; i++)
     {
         cnt[x][y][i] = 0;
-        pq.push({cnt[x][y][i], x, y, i});
+        dq.push_front({x, y, i});
     }
 
-    while(!pq.empty())
+    while(!dq.empty())
     {
-        auto [curCnt, curX, curY, curDir] = pq.top();
-        pq.pop();
+        auto [curX, curY, curDir] = dq.front();
+        dq.pop_front();
 
         int nX = curX + dx[curDir];
         int nY = curY + dy[curDir];
 
-        if(nX < 0 || nX >= n || nY < 0 || nY >= n) continue; // 범위 넘으면 패스
-        if(board[nX][nY] == '*') continue; // 벽이면 패스
+        if(nX < 0 || nX >= n || nY < 0 || nY >= n) continue;
+        if(board[nX][nY] == '*') continue;
 
-        // 같은 방향 진행
-        if(cnt[nX][nY][curDir] > curCnt)
+        
+        // 거울 설치 안하는 방법을 먼저 방문
+        if(cnt[nX][nY][curDir] > cnt[curX][curY][curDir])
         {
-            cnt[nX][nY][curDir] = curCnt;
-            pq.push({cnt[nX][nY][curDir], nX, nY, curDir});
+            cnt[nX][nY][curDir] = cnt[curX][curY][curDir];
+            dq.push_front({nX, nY, curDir});
         }
 
-        // 거울 위치 후보면 거울 설치
         if(board[nX][nY] == '!')
         {
-            
             int prevDir = (curDir - 1) % 4;
             if(prevDir < 0) prevDir += 4;
 
-            if(cnt[nX][nY][prevDir] > curCnt + 1)
+            if(cnt[nX][nY][prevDir] > cnt[curX][curY][curDir] + 1)
             {
-                cnt[nX][nY][prevDir] = curCnt + 1;
-                pq.push({cnt[nX][nY][prevDir], nX, nY, prevDir});
+                cnt[nX][nY][prevDir] = cnt[curX][curY][curDir] + 1;
+                dq.push_back({nX, nY, prevDir});
             }
 
-            int nxtDir = (curDir + 1) % 4;
+            int nDir = (curDir + 1) % 4;
 
-            if(cnt[nX][nY][nxtDir] > curCnt + 1)
+            if(cnt[nX][nY][nDir] > cnt[curX][curY][curDir] + 1)
             {
-                cnt[nX][nY][nxtDir] = curCnt + 1;
-                pq.push({cnt[nX][nY][nxtDir], nX, nY, nxtDir});
+                cnt[nX][nY][nDir] = cnt[curX][curY][curDir] + 1;
+                dq.push_back({nX, nY, nDir});
             }
         }
     }
@@ -114,7 +114,8 @@ int main()
         }
     }
 
-    Dijkstra(stX, stY);
+    // 0-1 BFS
+    BFS(stX, stY);
 
     int ans = INT_MAX;
     for(int i = 0; i < 4; i++)
