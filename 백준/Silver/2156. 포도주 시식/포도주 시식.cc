3a6066a0b7b13a26380e07d654a3e9c1b15452dd
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-11-20 09:23:23 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -30,7 +30,7 @@ int main()
     for(int i = 3; i <= n; i++)
     {
         dp[i][0] = max({dp[i-1][0], dp[i-1][1], dp[i-1][2]});
-        dp[i][1] = max({dp[i-2][0], dp[i-2][1], dp[i-2][2]}) + wine[i];
+        dp[i][1] = dp[i-1][0] + wine[i];
         dp[i][2] = dp[i-1][1] + wine[i];
 
         // cout << "dp[" << i << "][0]: " << dp[i][0] << "\n";
