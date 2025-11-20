3f8920f412c812744150bdd2406d83c2f20a77cc
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-11-20 09:12:56 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int wine[10001];
+
+int dp[10001][3]; // dp[i][j] : i번째 포도주까지 j번 연속으로 왔을 때 마신 최대 포도주 양
+
+int ans = 0;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        cin >> wine[i];
+    }
+
+    dp[1][1] = wine[1];
+
+    dp[2][0] = max({dp[1][0], dp[1][1], dp[1][2]});
+    dp[2][1] = wine[2];
+    dp[2][2] = dp[1][1] + wine[2];
+
+    for(int i = 3; i <= n; i++)
+    {
+        dp[i][0] = max({dp[i-1][0], dp[i-1][1], dp[i-1][2]});
+        dp[i][1] = max({dp[i-2][0], dp[i-2][1], dp[i-2][2]}) + wine[i];
+        dp[i][2] = dp[i-1][1] + wine[i];
+
+        // cout << "dp[" << i << "][0]: " << dp[i][0] << "\n";
+        // cout << "dp[" << i << "][1]: " << dp[i][1] << "\n";
+        // cout << "dp[" << i << "][2]: " << dp[i][2] << "\n";
+        // cout << "======================\n";
+
+    }
+
+    ans = max({dp[n][0], dp[n][1], dp[n][2]});
+
+    // cout << dp[n][0] << " " <<  dp[n][1] << " " << dp[n][2] << "\n";
+
+    cout << ans << "\n";
+
+    return 0;
+}
