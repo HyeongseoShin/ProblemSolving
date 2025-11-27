3825571af6f5dea33761a0ad8cb59f0fad6eddfc
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2025-11-27 09:49:02 +0900

    [Gold IV] Title: 트리 노드 합의 최댓값, Time: 48 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Gold/25515. 트리 노드 합의 최댓값/트리 노드 합의 최댓값.cc
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX 100001
+
+typedef long long ll;
+
+int n;
+
+vector<int> adj[MAX];
+
+ll weights[MAX];
+
+bool vis[MAX];
+
+// dfs(cur) : cur - 자식까지 가는데 방문 가능한 정수의 최대 합
+ll dfs(int cur)
+{
+    vis[cur] = true;
+
+    vector<ll> v;
+
+    for(auto nxt : adj[cur])
+    {
+        if(vis[nxt]) continue;
+        v.push_back(dfs(nxt));
+    }
+
+    int sz = (int)v.size();
+
+    ll sum = 0LL;
+
+    for(int i = 0; i < sz; i++)
+    {
+        sum = max(sum, sum + v[i]);
+    }
+
+    return sum + weights[cur];
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    for(int i = 0; i < n - 1; i++)
+    {
+        int p, c;
+        cin >> p >> c;
+
+        adj[p].push_back(c);
+        adj[c].push_back(p);
+    }
+
+    for(int i = 0; i < n; i++) cin >> weights[i];
+
+    cout << dfs(0) << "\n";
+
+    return 0;
+}
