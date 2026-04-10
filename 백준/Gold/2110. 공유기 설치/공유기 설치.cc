30caa495edf720983fc8c5525cbe52187b401bb7
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2026-04-10 22:28:03 +0900

    [Gold IV] Title: 공유기 설치, Time: 36 ms, Memory: 3684 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.cc
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.cc
@@ -4,7 +4,26 @@ using namespace std;
 
 int n, c;
 
-vector<int> home;
+vector<int> pos;
+
+int getCnt(int x)
+{
+    int cnt = 1;
+    int lastPos = 0;
+
+    for(int i = 1; i < n; i++)
+    {
+        if(pos[i] - pos[lastPos] >= x)
+        {
+            cnt++;
+            lastPos = i;
+        }
+
+        if(cnt == c) break;
+    }
+
+    return cnt;
+}
 
 int main()
 {
@@ -18,42 +37,29 @@ int main()
         int x;
         cin >> x;
 
-        home.push_back(x);
+        pos.push_back(x);
     }
 
-    sort(home.begin(), home.end());
+    sort(pos.begin(), pos.end());
 
-    int st = 0;
+    int l = 0;
+    int r = 1000000001;
 
-    // 48% 틀렸습니다 원인 : 원래 en = MAX였음
-    int en = home[n - 1] - home[0];
-    int ans = 0;
-
-    while(st <= en)
+    int ans = -1;
+    while(l < r)
     {
-        // 현재 가장 가까운 거리
-        int mid = (st + en) / 2;
+        int mid = (l + r) / 2;
+        int cnt = getCnt(mid);
 
-        // 첫 번째 집은 무조건 설치
-        int pos = home[0];
-        int cnt = 1;
-        
-        for(int i = 1; i < n; i++)
-        {
-            if(home[i] - pos >= mid)
-            {
-                pos = home[i];
-                cnt++;
-            }
-        }
+        // cout << "cnt: " << cnt << " mid: " << mid << "\n";
 
-        if(cnt < c) en = mid - 1;
+        if(cnt < c) r = mid;
         else
         {
-            ans = mid;
-            st = mid + 1;
+            ans = max(ans, mid);
+            l = mid + 1;
         }
-    }  
+    }
 
     cout << ans << "\n";
 
