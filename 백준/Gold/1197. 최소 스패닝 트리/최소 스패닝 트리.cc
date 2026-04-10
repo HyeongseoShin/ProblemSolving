970f3cc214662e4c0aceb4c9bc1b669c02d0bb76
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2026-04-11 00:14:24 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 36 ms, Memory: 4496 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -4,78 +4,54 @@ using namespace std;
 
 int v, e;
 
-tuple<int, int, int> edges[100005]; // (w, u, v)
-int parent[10005]; // 각 정점의 Parent 저장 (Union & Find 위함)
+int parent[10001];
 
 int Find(int v)
 {
-    if(parent[v] == v)
-    {
-        return parent[v];
-    }
-    
+    if(parent[v] == v) return parent[v];
     return parent[v] = Find(parent[v]);
 }
-
 
 void Union(int v1, int v2)
 {
     v1 = Find(v1);
     v2 = Find(v2);
 
-    if(v1 < v2)
-    {
-        parent[v2] = v1;
-    }
-
-    else if(v1 > v2)
-    {
-        parent[v1] = v2;
-    }
+    if(v1 < v2) parent[v2] = v1;
+    else parent[v1] = v2;
 }
 
-
+priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> edges;
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> v >> e;
-
     
-    for(int i = 1; i <= v; i++)
-    {
-        parent[i] = i;
-    }
-
     for(int i = 0; i < e; i++)
     {
-        cin >> get<1>(edges[i]) >> get<2>(edges[i]) >> get<0>(edges[i]);
-        
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        edges.push({c, a, b});
     }
 
-    sort(edges, edges + e);
+    for(int i = 1; i <= v; i++) parent[i] = i;
 
-    int cnt = 0;
     int ans = 0;
-
-    for(int i = 0; i < e; i++)
+    int cnt = 0;
+    while(cnt < v - 1)
     {
-        int u1, v1, w1;
-        tie(w1, u1, v1) = edges[i];
+        auto [w, from, to] = edges.top();
+        edges.pop();
 
-        if(Find(u1) != Find(v1))
+        if(Find(from) != Find(to))
         {
-            Union(u1, v1);
-            ans += w1;
+            Union(from, to);
             cnt++;
+            ans += w;
         }
-
-        if(cnt == v - 1)
-        {
-            break;
-        }
-        
     }
 
     cout << ans << "\n";
