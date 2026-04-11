4f91edddb2f80a5a1b1e74e8d371652bcbccfb0d
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2026-04-11 18:53:04 +0900

    [Gold II] Title: 원판 돌리기, Time: 4 ms, Memory: 2304 KB -BaekjoonHub

--- 백준/Gold/17822. 원판 돌리기/원판 돌리기.cc
+++ 백준/Gold/17822. 원판 돌리기/원판 돌리기.cc
@@ -2,117 +2,125 @@
 
 using namespace std;
 
-// 원판 번호, 원판에 있는 숫자의 수, 회전 횟수
+// 원판 , 정수, 테스트 수
 int n, m, t;
 
-int board[53][53];
+int board[51][51];
 
-int ans = 0;
+// 회전 방법
+// 번호 => x배수인 원판
+// d => 방향 0: 시계, 1: 반시계
+// k 칸만큼
+int x, d, k;
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
-// d : 0 => 시계 방향
-// d : 1 => 반시계 방향
-void rotate(int x, int d, int k)
+
+void printBoard()
 {
-    int tmp[53];
-
-    for(int i = 0; i < m; i++)
+    cout << "\n========Board==========\n";
+    for(int i = 0; i < n; i++)
     {
-        tmp[i] = board[x][i];
-    }
-
-    int dir = d;
-
-    // 시계 방향이면 진행 방향 -1
-    if(dir == 0) dir = -1;
-
-    for(int i = 0; i < m; i++)
-    {
-        int idx = (i + (dir * k)) % m;
-        if(idx < 0) idx += m;
-
-        board[x][i] = tmp[idx];
+        for(int j = 0; j < m; j++)
+        {
+            cout << board[i][j] << " ";
+        }
+        cout << "\n";
     }
 }
 
-void deleteNearNum()
+// 원판 회전
+void rotateBoard(int idx, int dir, int cnt)
 {
-    set<pair<int, int>> delPos;
-    for(int i = 0; i < n; i++)
+    int tmpBoard[51];
+    memcpy(tmpBoard, board[idx], sizeof(board[idx]));
+
+    int step = 1;
+    // 반시계 방향
+    if(dir == 1) step = -1;
+
+    for(int i = 0; i < m; i++)
     {
-        for(int j = 0; j < m; j++)
-        {
-            bool isDeleted = false;
+        int nxt = (i + (k * step)) % m;
+        if(nxt < 0) nxt += m;
 
-            // 삭제된 수이면 패스
-            if(board[i][j] == 0) continue;
-
-            for(int dir = 0; dir < 4; dir++)
-            {
-                int nX = i + dx[dir];
-                int nY = (j + dy[dir]) % m;
-
-                if(nX < 0 || nX >= n || nY < 0 || nY >= m) continue;
-
-                if(board[i][j] == board[nX][nY])
-                {
-                    delPos.insert({nX, nY});
-                    isDeleted = true;
-                }
-            }
-
-            if(isDeleted)
-            {
-                delPos.insert({i, j});
-            }
-        }
+        board[idx][nxt] = tmpBoard[i];
     }
+}
 
-    // 삭제되어야 할 수 삭제
-    if((int)delPos.size() > 0)
-    {
-        for(auto [i, j] : delPos)
-        {
-            board[i][j] = 0;
-        }
-
-        return;
-    }
-    
-    // 삭제해야 할 수 없다면 평균 구하기
-    double avg = 0.0;
+double getAvg()
+{
+    double result = 0.0;
     int sum = 0;
     int cnt = 0;
-    
+
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < m; j++)
         {
             if(board[i][j] == 0) continue;
-
             sum += board[i][j];
             cnt++;
         }
     }
-    
 
-    // 평균보다 큰 수--, 작은 수++
-    avg = (double)sum / (double)cnt;
+    result = (double) sum / (double) cnt;
 
+    return result;
+}
+
+void check()
+{
+    bool isDeleted = false;
+    vector<pair<int, int>> deletePos;
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < m; j++)
         {
             if(board[i][j] == 0) continue;
 
-            if(board[i][j] > avg) board[i][j]--;
-            
-            else if(board[i][j] < avg) board[i][j]++;
+            bool isSame = false;
+            for(int dir = 0; dir < 4; dir++)
+            {
+                int nX = i + dx[dir];
+                int nY = (j + dy[dir]) % m;
+                if(nY < 0) nY += m;
+
+                if(nX < 0 || nX >= n || nY < 0 || nY >= m) continue;
+                if(board[nX][nY] == board[i][j])
+                {
+                    deletePos.push_back({nX, nY});
+                    isSame = true;
+                    isDeleted = true;
+                }
+            }
+
+            if(isSame) deletePos.push_back({i, j});
+        }
+    }
+
+    // 한 꺼번에 모아서 지우기
+    for(auto [delX, delY] : deletePos) board[delX][delY] = 0;
+    
+
+    if(!isDeleted)
+    {
+        double avg = getAvg();
+        // cout << "avg: " << avg << "\n";
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(board[i][j] == 0) continue;
+                if((double)board[i][j] > avg) board[i][j]--;
+                else if((double)board[i][j] < avg) board[i][j]++;
+            }
         }
     }
 }
+
 
 int main()
 {
@@ -129,21 +137,17 @@ int main()
         }
     }
 
-    while(t--)
+    for(int i = 0; i < t; i++)
     {
-        int x, d, k;
-        
         cin >> x >> d >> k;
 
-        // 0부터 시작해서 x만큼 ++ 하면 0부터 시작해도 x의 배수인 원판만 회전 가능
-        for(int i = 0 + (x - 1); i < n; i += x)
-        {
-            rotate(i, d, k);
-        }
+        for(int j = (x - 1); j < n; j += x) rotateBoard(j, d, k);        
+        check();
 
-        deleteNearNum(); // 인접한 수 있는지 확인 후 삭제
+        // printBoard();
     }
 
+    int ans = 0;
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < m; j++)
