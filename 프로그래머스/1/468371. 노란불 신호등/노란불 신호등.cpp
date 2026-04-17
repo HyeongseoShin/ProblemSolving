b2236a06d95b6052f1a7878824f52a3348ded5ad
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2026-04-17 22:31:12 +0900

    [level 1] Title: 노란불 신호등, Time: 3.87 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.cpp
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.cpp
@@ -5,31 +5,55 @@ using namespace std;
 int n;
 int ans = -1;
 
-int yCnt[10000001];
+int Lcm = 1;
+
+int getGcd(int a, int b)
+{
+    if(a == 0) return b;
+    return getGcd(b % a, a);
+}
+
+int getLcm(int a, int b)
+{
+    return a / getGcd(a, b) * b;
+}
+
 
 int solution(vector<vector<int>> signals) {
     n = (int)signals.size();
     
+    // 최소 공배수 구하기
     for(int i = 0; i < n; i++)
     {
         int g = signals[i][0];
         int y = signals[i][1];
         int r = signals[i][2];
         
-        int st = g + 1;
-        int step = g + y + r;
-        
-        for(int j = st; j + y < 10000001; j += step)
-        {
-            yCnt[j] += 1;
-            yCnt[j + y] -= 1;
-        }
+        Lcm = getLcm(Lcm, g + y + r);
     }
     
-    for(int i = 1; i < 10000001; i++)
+    // 현재 시각
+    for(int i = 1; i <= Lcm; i++)
     {
-        yCnt[i] = yCnt[i-1] + yCnt[i];
-        if(yCnt[i] == n)
+        bool isPossible = true;
+        for(int j = 0; j < n; j++)
+        {
+            int g = signals[j][0];
+            int y = signals[j][1];
+            int r = signals[j][2];
+            int cycle = g + y + r;
+            
+            // 현재 주기에서 위치
+            int cur = (i - 1) % cycle + 1;
+            
+            if(!(g + 1 <= cur && cur <= g + y))
+            {
+                isPossible = false;
+                break;
+            }
+        }
+        
+        if(isPossible)
         {
             ans = i;
             break;
