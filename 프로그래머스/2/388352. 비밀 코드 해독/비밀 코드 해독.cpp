5d859f1d2a3f14f2940b2526e8955c9569a98568
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2026-05-01 01:18:33 +0900

    [level 2] Title: 비밀 코드 해독, Time: 7.04 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.cpp
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.cpp
@@ -4,9 +4,43 @@ using namespace std;
 
 int answer = 0;
 
-int solution(int n, vector<vector<int>> q, vector<int> ans) {
+vector<int> curNums;
 
-    cout << pow(3, 5) * pow(10, 5) << "\n";
+bool checkPossible(vector<vector<int>> &q, vector<int> &ans)
+{
+    for(int i = 0; i < (int)q.size(); i++)
+    {
+        int cnt = 0;
+        for(int j = 0; j < (int)curNums.size(); j++)
+        {
+            if(find(q[i].begin(), q[i].end(), curNums[j]) != q[i].end()) cnt++;
+        }
+        
+        if(ans[i] != cnt) return false;
+    }
+    
+    return true;
+}
+
+void dfs(int cur, int n, vector<vector<int>> &q, vector<int> &ans)
+{
+    if((int)curNums.size() == 5)
+    {
+        if(checkPossible(q, ans)) answer++;
+        return;
+    }
+    
+    for(int i = cur; i <= n; i++)
+    {
+        curNums.push_back(i);
+        dfs(i + 1, n, q, ans);
+        curNums.pop_back();
+    }
+}
+
+int solution(int n, vector<vector<int>> q, vector<int> ans) {
+    
+    dfs(1, n, q, ans);
     
     return answer;
 }
