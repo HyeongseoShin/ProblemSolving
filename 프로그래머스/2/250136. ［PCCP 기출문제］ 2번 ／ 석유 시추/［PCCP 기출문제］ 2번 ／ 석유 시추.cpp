7529aaa9324631906495846db72f57e6722b692f
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2026-04-30 17:16:52 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 12.81 ms, Memory: 14.3 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
@@ -5,12 +5,11 @@ using namespace std;
 int n, m;
 int ans = 0;
 
-int board[501][501];
+int board[501][501]; // 진행 판
 
-// 열마다 뽑을 수 있는 석유 크기
-int oilSz[501];
+int oilSz[501]; // 열마다 뽑을 수 있는 석유 크기
 
-bool vis[501][501];
+bool vis[501][501]; // 방문 확인 배열
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
@@ -18,12 +17,12 @@ int dy[4] = {0, 0, -1, 1};
 void bfs(int x, int y)
 {
     vis[x][y] = true;
-    queue<pair<int, int>> q;
     
+    queue<pair<int, int>> q;
     q.push({x, y});
     
-    unordered_set<int> cols; // 현재 석유가 뽑힐 수 있는 열 번호 목록
-    cols.insert(y);
+    set<int> cols; // 현재 석유가 뽑힐 수 있는 열 번호 목록
+    cols.insert(y); // 시작 위치 열 저장
     
     int cnt = 1; // 현재 석유의 크기
     
@@ -37,31 +36,25 @@ void bfs(int x, int y)
             int nX = curX + dx[i];
             int nY = curY + dy[i];
             
-            if(nX < 0 || nX >= n || nY < 0 || nY >= m) continue;
-            if(vis[nX][nY]) continue;
-            if(board[curX][curY] != board[nX][nY]) continue;
+            if(nX < 0 || nX >= n || nY < 0 || nY >= m) continue; // 범위 넘으면 패스
+            if(vis[nX][nY]) continue; // 방문했으면 패스
+            if(board[nX][nY] == 0) continue; // 벽이면 패스
             
-            cols.insert(nY);
-            cnt++;
+            cnt++; // 석유 크기 증가
+            cols.insert(nY); // 석유 뽑힐 수 있는 열 추가
+            
             vis[nX][nY] = true;
             q.push({nX, nY});
         }
     }
     
-    // cout << "cnt: " << cnt << " - ";
-    for(auto col : cols)
-    {
-        // cout << col << " ";
-        oilSz[col] += cnt;
-    }
-    // cout << "\n";
+    // 뽑힐 수 있는 열들에 현재 석유 크기 업데이트
+    for(auto col : cols) oilSz[col] += cnt;
 }
 
 int solution(vector<vector<int>> land) {
     n = (int)land.size();
     m = (int)land[0].size();
-    
-    // cout << "n: " << n << " m: " << m << "\n";
     
     for(int i = 0; i < n; i++)
     {
@@ -75,17 +68,14 @@ int solution(vector<vector<int>> land) {
     {
         for(int j = 0; j < m; j++)
         {
-            if(board[i][j] == 0) continue;
-            if(vis[i][j]) continue;
+            if(board[i][j] == 0) continue; // 벽이면 패스
+            if(vis[i][j]) continue; // 이미 방문했으면 패스
             bfs(i, j);
         }
     }
     
-    for(int i = 0; i < m; i++)
-    {
-        // cout << oilSz[i] << "\n";
-        ans = max(ans, oilSz[i]);
-    }
+    // 석유 가장 많이 뽑을 수 있는 열 선택
+    for(int i = 0; i < m; i++) ans = max(ans, oilSz[i]);
     
     return ans;
 }
